eb82bea3e58568ea4639fe67a66c575eadcc0e82
Montana Carlozo <mcarlozo@nd.edu>
2022-02-10 13:33:36 -0500

    Simplified best_error function

--- bo_functions.py
+++ bo_functions.py
@@ -1,11 +1,12 @@
-def calc_best_error(test_p, x, y_true, y_model, noise):
+import numpy as np
+
+def calc_best_error(test_p, x, y_model, noise):
     """
     Calculates the best error in the GP model
     
     Parameters
     ----------
         test_p: ndarray: The parameter space for which the best error is being calculated
-        y_true: ndarray: The actual value of the model (based on data or otherwise)
         y_model: ndarray: The y values that the GP model predicts 
         noise: ndarray: The noise associated with the experimental value of the model
     
@@ -13,28 +14,11 @@ def calc_best_error(test_p, x, y_true, y_model, noise):
     --------
         best_error: float, the value of the best error encountered  
      """ 
-    error = np.zeros(len(test_p)) #1 x 25
-    y_real = np.zeros(len(test_p)) #1 x 25
-
-    for i in range(len(test_x)):
+    for i in range(len(test_p)):
         test_p_1 = test_p[i,0] #5x1 
         test_p_2 = test_p[i,1] #5x1
         #Calculates actual y value for each parameter space combination
-        y_exp = test_p_1*x + test_p_2*x**2 +x**3 + noise #100 x5
-        #Ccalculates the actual y for each p combo
-        y_real[i] = sum((y_true - y_exp)**2) # A number
-        #Calculates error of model
-        error[i] = (y_real[i]-y_model[i])**2 #A number
-        if j == 0:
-            #Sets first error to be the best error
-            best_error = -error[i]
-            best_p = test_p[np.argmax(-error[i])]
-        else:
-            #Overwrites a new value of best error as error at each theta combo is updated
-            best_error = np.max(-error)
-            best_p = test_p[np.argmax(-error)]
-        #Makes best_error positive again
-        best_error = -best_error
+        best_error = np.argmax(y_model)
     return best_error
 
 def calc_ei_basic(f_best,pred_mean,pred_var,explore_bias):
@@ -83,3 +67,27 @@ def calc_ei_basic(f_best,pred_mean,pred_var,explore_bias):
         
 #         EI[j] = ei_term1 + ei_term2 + ei_term3
 #     return EI
+
+    
+#     error = np.zeros(len(test_p)) #1 x 25
+#     y_real = np.zeros(len(test_p)) #1 x 25
+#     for i in range(len(test_p)):
+#         test_p_1 = test_p[i,0] #5x1 
+#         test_p_2 = test_p[i,1] #5x1
+#         #Calculates actual y value for each parameter space combination
+#         y_exp = test_p_1*x + test_p_2*x**2 +x**3 + noise #100 x5
+#         #Ccalculates the actual y for each p combo
+#         y_real[i] = sum((y_true - y_exp)**2) # A number 
+#         #Calculates error of model
+#         error[i] = (y_real[i]-y_model[i])**2 #A number
+#         if i == 0:
+#             #Sets first error to be the best error
+#             best_error = -error[i]
+#             best_p = test_p[np.argmax(-error[i])]
+#         else:
+#             #Overwrites a new value of best error as error at each theta combo is updated
+#             best_error = np.max(-error)
+#             best_p = test_p[np.argmax(-error)]
+#         #Makes best_error positive again
+#         best_error = -best_error
+#     return best_error
