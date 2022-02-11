1893eb7d153ad493b556d0e1b9837fcd18006e34
Montana Carlozo <mcarlozo@nd.edu>
2022-02-10 20:53:31 -0500

    Added more data points, updated advanced functions, and implemented them into the GP model

--- bo_functions.py
+++ bo_functions.py
@@ -67,17 +67,14 @@ def best_error_advanced(test_p, y_model, y_true, noise):
         best_error: float, the value of the best error encountered 
     """
     error = np.zeros(len(test_p)) #1 x 25
-    y_real = np.zeros(len(test_p)) #1 x 25
     for i in range(len(test_p)):
         test_p_1 = test_p[i,0] #5x1 (Theta 1) 
         test_p_2 = test_p[i,1] #5x1(Theta 2)
-        test_p_3 = test_p[i,3] #5x1 (x)
-        #Calculates actual y value for each parameter space combination
-        y_exp = test_p_1*test_p_3 + test_p_2*test_p_3**2 +test_p_3**3 #+ noise #100 x5 (will need to redefine noise)
-        #Ccalculates the actual y for each p combo
-        y_real[i] = sum((y_true - y_exp)**2) # A number 
+        test_p_3 = test_p[i,2] #5x1 (x)
+        #Calculates expected y value for each parameter space combination
+        y_exp = test_p_1*test_p_3 + test_p_2*test_p_3**2 +test_p_3**3 + noise #100 x5 (may need to redefine noise)
         #Calculates error of model
-        error[i] = (y_real[i]-y_model[i])**2 #A number
+        error[i] = (y_true[i]-y_model[i])**2 #A number
         if i == 0:
             #Sets first error to be the best error
             best_error = -error[i]
@@ -89,36 +86,36 @@ def best_error_advanced(test_p, y_model, y_true, noise):
         #Makes best_error positive again
         best_error = -best_error
     return best_error
-    
+  
 
 
-def cacl_ei_advanced(f_best,pred_mean,pred_var, explore_bias):
-    EI = np.zeros(len(test_T)) # 1 x 25
+def calc_ei_advanced(f_best,pred_mean,pred_var, y_true):
+    ei = np.zeros(len(pred_mean)) # 1 x 6
     #If variance is zero this is important
+    for i in range(len(pred_mean)):
+        with np.errstate(divide = 'warn'):
+            #Creates upper and lower bounds and described by Nilay's word doc
+            bound_upper = ((y_true[i] - pred_mean[i]) +np.sqrt(f_best))/pred_var[i]
+            bound_lower = ((y_true[i] - pred_mean[i]) -np.sqrt(f_best))/pred_var[i]
 
-    with np.errstate(divide = 'warn'):
-        #Creates upper and lower bounds and described by Nilay's word doc
-        bound_upper = ((sse_true[j] - model_mean[j]) +np.sqrt(best_error))/model_variance[j]
-        bound_lower = ((sse_true[j] - model_mean[j]) -np.sqrt(best_error))/model_variance[j]
-        
-        #Creates EI terms in terms of Nilay's word doc
-        ei_term1_comp1 = norm.cdf(bound_upper) - norm.cdf(bound_lower)
-        ei_term1_comp2 = best_error - (sse_true[j] - model_mean[j])**2
-        
-        ei_term2_comp1 = (sse_true[j] - model_mean[j])*model_stdev[j]
-        ei_term2_comp2 = norm.pdf(bound_upper) - norm.pdf(bound_lower)
-        
-        ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2))
-        ei_term3_comp2 = -norm.pdf(bound_upper)*bound_upper
-        ei_term3_comp3 = (1/2)*norm.pdf(bound_lower/np.sqrt(2))
-        ei_term3_comp4 = -norm.pdf(bound_lower)*bound_lower
-        
-        ei_term3_psi_upper = ei_term3_comp1 + ei_term3_comp2
-        ei_term3_psi_lower = ei_term3_comp3 + ei_term3_comp4
-        
-        ei_term1 = ei_term1_comp1 + ei_term1_comp2
-        ei_term2 = ei_term2_comp1 + ei_term2_comp2
-        ei_term3 = -model_variance[j]*(ei_term3_psi_upper-ei_term3_psi_lower)
-        
-        EI[j] = ei_term1 + ei_term2 + ei_term3
-    return EI
+            #Creates EI terms in terms of Nilay's word doc
+            ei_term1_comp1 = norm.cdf(bound_upper) - norm.cdf(bound_lower)
+            ei_term1_comp2 = f_best - (y_true[i] - pred_mean[i])**2
+
+            ei_term2_comp1 = (y_true[i] - pred_mean[i])*np.sqrt(pred_var[i])
+            ei_term2_comp2 = norm.pdf(bound_upper) - norm.pdf(bound_lower)
+
+            ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2))
+            ei_term3_comp2 = -norm.pdf(bound_upper)*bound_upper
+            ei_term3_comp3 = (1/2)*norm.pdf(bound_lower/np.sqrt(2))
+            ei_term3_comp4 = -norm.pdf(bound_lower)*bound_lower
+
+            ei_term3_psi_upper = ei_term3_comp1 + ei_term3_comp2
+            ei_term3_psi_lower = ei_term3_comp3 + ei_term3_comp4
+
+            ei_term1 = ei_term1_comp1 + ei_term1_comp2
+            ei_term2 = ei_term2_comp1 + ei_term2_comp2
+            ei_term3 = -pred_var[i]*(ei_term3_psi_upper-ei_term3_psi_lower)
+
+            ei[i] = ei_term1 + ei_term2 + ei_term3
+        return ei
