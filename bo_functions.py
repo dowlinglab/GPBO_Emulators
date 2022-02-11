c38265b2f861558044155a35dbca73f37cf60b50
Montana Carlozo <mcarlozo@nd.edu>
2022-02-11 12:51:57 -0500

    Added a factor of 2 that was missing in function and updated gp model after consolidating function code

--- bo_functions.py
+++ bo_functions.py
@@ -50,76 +50,6 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
             ei = 0
     return ei
 
-
-# def best_error_advanced(test_p, y_model, y_true, noise):
-    
-#     """
-#     Calculates the best error in the 3 input GP model
-    
-#     Parameters
-#     ----------
-#         test_p: ndarray: The parameter space for which the best error is being calculated
-#         y_model: ndarray: The y values that the GP model predicts 
-#         noise: ndarray: The noise associated with the experimental value of the model
-    
-#     Returns:
-#     --------
-#         best_error: float, the value of the best error encountered 
-#     """
-#     error = np.zeros(len(test_p)) #1 x 25
-#     for i in range(len(test_p)):
-#         test_p_1 = test_p[i,0] #5x1 (Theta 1) 
-#         test_p_2 = test_p[i,1] #5x1(Theta 2)
-#         test_p_3 = test_p[i,2] #5x1 (x)
-#         #Calculates expected y value for each parameter space combination
-#         y_exp = test_p_1*test_p_3 + test_p_2*test_p_3**2 +test_p_3**3 + noise #100 x5 (may need to redefine noise)
-#         #Calculates error of model
-#         error[i] = (y_true[i]-y_model[i])**2 #A number
-#         if i == 0:
-#             #Sets first error to be the best error
-#             best_error = -error[i]
-#             best_p = test_p[np.argmax(-error[i])]
-#         else:
-#             #Overwrites a new value of best error as error at each theta combo is updated
-#             best_error = np.max(-error)
-#             best_p = test_p[np.argmax(-error)]
-#         #Makes best_error positive again
-#         best_error = -best_error
-#     return best_error
-  
-
-
-# def calc_ei_advanced(f_best,pred_mean,pred_var, y_true):
-#     ei = np.zeros(len(pred_mean)) # 1 x 6
-#     #If variance is zero this is important
-#     for i in range(len(pred_mean)):
-#         with np.errstate(divide = 'warn'):
-#             #Creates upper and lower bounds and described by Nilay's word doc
-#             bound_upper = ((y_true[i] - pred_mean[i]) +np.sqrt(f_best))/pred_var[i]
-#             bound_lower = ((y_true[i] - pred_mean[i]) -np.sqrt(f_best))/pred_var[i]
-
-#             #Creates EI terms in terms of Nilay's word doc
-#             ei_term1_comp1 = norm.cdf(bound_upper) - norm.cdf(bound_lower)
-#             ei_term1_comp2 = f_best - (y_true[i] - pred_mean[i])**2
-
-#             ei_term2_comp1 = (y_true[i] - pred_mean[i])*np.sqrt(pred_var[i])
-#             ei_term2_comp2 = norm.pdf(bound_upper) - norm.pdf(bound_lower)
-
-#             ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2))
-#             ei_term3_comp2 = -norm.pdf(bound_upper)*bound_upper
-#             ei_term3_comp3 = (1/2)*norm.pdf(bound_lower/np.sqrt(2))
-#             ei_term3_comp4 = -norm.pdf(bound_lower)*bound_lower
-
-#             ei_term3_psi_upper = ei_term3_comp1 + ei_term3_comp2
-#             ei_term3_psi_lower = ei_term3_comp3 + ei_term3_comp4
-
-#             ei_term1 = ei_term1_comp1 + ei_term1_comp2
-#             ei_term2 = ei_term2_comp1 + ei_term2_comp2
-#             ei_term3 = -pred_var[i]*(ei_term3_psi_upper-ei_term3_psi_lower)
-
-#             ei[i] = ei_term1 + ei_term2 + ei_term3
-#         return ei
-
 import numpy as np
 from scipy.stats import norm
 
@@ -188,14 +118,7 @@ def best_error_advanced(test_p, y_model, y_true, noise):
     --------
         best_error: float, the value of the best error encountered 
     """
-    #Separates parameters for use
-    test_p_1 = test_p[:,0] #Theta1 #1 x 6
-    test_p_2 = test_p[:,1] #Theta 2 #1 x 6
-    test_p_3 = test_p[:,2] #x #1 x 6
-    
-    #Calculates error for each parameter space combination
-    y_exp = (test_p_1*test_p_3 + test_p_2*test_p_3**2 +test_p_3**3 + noise).numpy() #1 x6 (may need to redefine noise)
-    error = (y_exp-y_model)**2 #1x6
+    error = (y_true-y_model)**2 #1x6
     best_error = np.max(-error) #A number
     best_p = test_p[np.argmax(-error)] #1x3
     
@@ -205,7 +128,7 @@ def best_error_advanced(test_p, y_model, y_true, noise):
     return best_error, best_p #1x2
 
 
-def calc_ei_advanced(f_best,pred_mean,pred_var,noise):
+def calc_ei_advanced(f_best,pred_mean,pred_var,y_true):
     """ 
     Calculates the expected improvement of the 3 input parameter GP
     Parameters
@@ -222,7 +145,6 @@ def calc_ei_advanced(f_best,pred_mean,pred_var,noise):
     #If variance is zero this is important
     pred_mean = pred_mean.numpy()
     pred_var = pred_var.numpy()
-    y_true = (test_p_1*test_p_3 + test_p_2*test_p_3**2 +test_p_3**3 + noise).numpy() #Added this line
     with np.errstate(divide = 'warn'):
         #Creates upper and lower bounds and described by Nilay's word doc
         bound_upper = ((y_true - pred_mean) +np.sqrt(f_best))/pred_var
@@ -232,7 +154,7 @@ def calc_ei_advanced(f_best,pred_mean,pred_var,noise):
         ei_term1_comp1 = norm.cdf(bound_upper) - norm.cdf(bound_lower)
         ei_term1_comp2 = f_best - (y_true - pred_mean)**2
 
-        ei_term2_comp1 = (y_true - pred_mean)*np.sqrt(pred_var)
+        ei_term2_comp1 = 2*(y_true - pred_mean)*np.sqrt(pred_var)
         ei_term2_comp2 = norm.pdf(bound_upper) - norm.pdf(bound_lower)
 
         ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2))
