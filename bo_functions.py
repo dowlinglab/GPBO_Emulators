6024627fd949c7375d2a790bab6c3de95cd61c26
Montana Carlozo <mcarlozo@nd.edu>
2022-02-11 13:56:50 -0500

    Updated basic function

--- bo_functions.py
+++ bo_functions.py
@@ -37,17 +37,18 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
         ei: ndarray, the expected improvement of the GP model
     
     """
+    ei = np.zeros(len(pred_var)) #1x25
     pred_mean = pred_mean.numpy() #1x25
     pred_var = pred_var.numpy()    #1x25
     pred_stdev = np.sqrt(pred_var) #1x25
-    for i in range(len(pred_mean)):
+    for i in range(len(pred_var)):
         if pred_stdev[i] > 0:
-            z = (pred_mean - f_best - explore_bias)/pred_stdev #1x25
-            ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #1 x 25
-            ei_term_2 = pred_stdev*norm.pdf(z) #1 x 25
-            ei = ei_term_1 +ei_term_2
+            z = (pred_mean[i] - f_best - explore_bias)/pred_stdev[i] #number
+            ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #number
+            ei_term_2 = pred_stdev[i]*norm.pdf(z) #number
+            ei[i] = ei_term_1 +ei_term_2
         else:
-            ei = 0
+            ei[i] = 0
     return ei
 
 
