4bb7fc97e4664c168b160edda1b0675dda0e5166
Montana Carlozo <mcarlozo@nd.edu>
2022-02-14 13:30:09 -0500

    Added comments to bo_functions

--- bo_functions.py
+++ bo_functions.py
@@ -16,18 +16,25 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
         ei: ndarray, the expected improvement of the GP model
     
     """
+    #Creates empty list to store ei values
     ei = np.zeros(len(pred_var)) #1x25
+    
+    #Converts tensors to np arrays and defines standard deviation
     pred_mean = pred_mean.numpy() #1x25
     pred_var = pred_var.numpy()    #1x25
     pred_stdev = np.sqrt(pred_var) #1x25
+    
+    #Loops over every standard deviation values
     for i in range(len(pred_var)):
+        #Checks that all standard deviations are positive
         if pred_stdev[i] > 0:
+            #Calculates ei based on Ke's formula
             z = (pred_mean[i] - f_best - explore_bias)/pred_stdev[i] #number
-            print(z)
             ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #number
             ei_term_2 = pred_stdev[i]*norm.pdf(z) #number
-            ei[i] = ei_term_1 +ei_term_2
+            ei[i] = ei_term_1 +ei_term_2 #A number
         else:
+            #Sets ei to zero if standard deviation is zero
             ei[i] = 0
     return ei
 
@@ -47,8 +54,11 @@ def best_error_advanced(test_p, y_model, y_target):
     --------
         best_error: float, the value of the best error encountered 
     """
+    #Calculates best error as the maximum of the -error
     error = (y_target-y_model)**2 #1x6
     best_error = np.max(-error) #A number
+    
+    #Finds the best x index
     best_x = np.argmax(-error) #1x3
     
     #Makes best_error positive again
@@ -64,17 +74,21 @@ def calc_ei_advanced(f_best,pred_mean,pred_var,y_target):
     ----------
         f_best: float, the best predicted error encountered
         pred_mean: tensor, model mean
-        pred_var, tensor, model variance
+        pred_var: tensor, model variance
         y_target: ndarray, the expected value of the function from data or other source
     
     Returns
     -------
         ei: ndarray, the expected improvement of the GP model
     """
-    #If variance is zero this is important
+    #Coverts tensor to np arrays
     pred_mean = pred_mean.numpy()
     pred_var = pred_var.numpy()
+    
+    #Defines standard devaition
     pred_stdev = np.sqrt(pred_var)
+    
+    #If variance is zero this is important
     with np.errstate(divide = 'warn'):
         #Creates upper and lower bounds and described by Nilay's word doc
         bound_upper = ((y_target - pred_mean) +np.sqrt(f_best))/pred_stdev
