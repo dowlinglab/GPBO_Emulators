1ebd3f5b7fbdcd576581cd56d07eab61eb493bd1
Montana Carlozo <mcarlozo@nd.edu>
2022-02-17 11:23:27 -0500

    Fixed bo_plotters to reduce code redundancy

--- bo_plotters.py
+++ bo_plotters.py
@@ -1,8 +1,6 @@
 from matplotlib import pyplot as plt
 
-y_title = plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
-
-def basic_plotter(test_mesh, z, p_true, p_GP_opt):
+def basic_plotter(test_mesh, z, p_true, p_GP_opt,title):
     '''
     Plots heat maps for 2 input GP
     '''
@@ -29,63 +27,7 @@ def basic_plotter(test_mesh, z, p_true, p_GP_opt):
     #Creates axis labels and title
     plt.xlabel('$\\theta_1$',weight='bold')
     plt.ylabel('$\\theta_2$',weight='bold')
-    plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
-
-    #Shows plot
-    return plt.show()
-
-y_title = plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
-
-def stdev_plotter(test_mesh, z, p_true, p_GP_opt):
-
-    xx , yy = test_mesh
-
-    #Plots Theta1 vs Theta 2 with sse on the z axis and plots the color bar
-    #Plot stdev.T because test_mesh.T was used to calculate stdev
-    plt.contourf(xx,yy,z.T)
-    plt.colorbar()
-
-    #Plots the true optimal value and the GP value
-    plt.scatter(p_true[0],p_true[1], color="red", label = "True", s=50)
-    plt.scatter(p_GP_opt[0],p_GP_opt[1], color="orange", label = "GP")
-
-    #Plots axes such that they are scaled the same way (eg. circles look like circles)
-    plt.axis('scaled')
-
-    #Plots grid and legend
-    plt.grid()
-    plt.legend(loc = 'best')
-
-    #Creates axis labels and title
-    plt.xlabel('Theta 1',weight='bold')
-    plt.ylabel('Theta 2',weight='bold')
-    plt.title('Heat Map of Standard Deviation', weight='bold',fontsize = 16)
-
-    #Shows plot
-    return plt.show()
-
-def ei_plotter(test_mesh, z, p_true, p_GP_opt):
-    xx , yy = test_mesh
-    
-    #Plots EI
-    plt.contourf(xx, yy,z.T)
-    plt.colorbar()
-
-    #Plots axes such that they are scaled the same way (eg. circles look like circles)
-    plt.axis('scaled')
-    
-    #Plots the true optimal value and the GP value
-    plt.scatter(p_true[0],p_true[1], color="red", label = "True", s=50)
-    plt.scatter(p_GP_opt[0],p_GP_opt[1], color="orange", label = "GP")
-    
-    #Plots grid and legend
-    plt.grid()
-    plt.legend(loc = 'best')
-
-    #Creates axis labels and title
-    plt.xlabel('Theta 1',weight='bold')
-    plt.ylabel('Theta 2',weight='bold')
-    plt.title('Heat Map of Expected Improvement', weight='bold',fontsize = 16)
+    plt.title("Heat Map of "+title, weight='bold',fontsize = 16)
 
     #Shows plot
     return plt.show()
