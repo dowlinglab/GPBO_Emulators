48880d7867123ce94b6757cb26237fe849feb037
Montana Carlozo <mcarlozo@nd.edu>
2022-02-17 11:07:30 -0500

    Updated files based on code review

--- bo_functions.py
+++ bo_functions.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.stats import norm
+import torch
 
-def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
+def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias=0.0):
     """ 
     Calculates the expected improvement of the 2 input parameter GP
     Parameters
@@ -9,31 +10,37 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
         f_best: float, the best predicted sse encountered
         pred_mean: tensor, model mean
         pred_var, tensor, model variance
-        explore_bias: float, the numerical bias towards exploration
+        explore_bias: float, the numerical bias towards exploration, zero is the default
     
     Returns
     -------
         ei: ndarray, the expected improvement of the GP model
     
     """
+    assert len(pred_mean) == len(pred_var), "GP predicted means and variances must be the same length"
+    assert torch.is_tensor(pred_mean)==True and torch.is_tensor(pred_var)==True, "GP predicted means and variances must be tensors"
+    
     #Creates empty list to store ei values
-    ei = np.zeros(len(pred_var)) #1x25
+    ei = np.zeros(len(pred_var)) #1xn
     
     #Converts tensors to np arrays and defines standard deviation
-    pred_mean = pred_mean.numpy() #1x25
-    pred_var = pred_var.numpy()    #1x25
-    pred_stdev = np.sqrt(pred_var) #1x25
+    pred_mean = pred_mean.numpy() #1xn
+    pred_var = pred_var.numpy()    #1xn
+    pred_stdev = np.sqrt(pred_var) #1xn
+  
     
     #Loops over every standard deviation values
     for i in range(len(pred_var)):
         #Checks that all standard deviations are positive
         if pred_stdev[i] > 0:
             #Calculates z-score based on Ke's formula
-            z = (pred_mean[i] - f_best - explore_bias)/pred_stdev[i] #number
+            z = (pred_mean[i] - f_best - explore_bias)/pred_stdev[i] #scaler
             #Calculates ei based on Ke's formula
-            ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #number
-            ei_term_2 = pred_stdev[i]*norm.pdf(z) #number
-            ei[i] = ei_term_1 +ei_term_2 #A number
+            #Explotation term
+            ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #scaler
+            #Exploration Term
+            ei_term_2 = pred_stdev[i]*norm.pdf(z) #scaler
+            ei[i] = ei_term_1 +ei_term_2 #scaler
         else:
             #Sets ei to zero if standard deviation is zero
             ei[i] = 0
--- bo_plotters.py
+++ bo_plotters.py
@@ -1,8 +1,11 @@
 from matplotlib import pyplot as plt
 
-def y_plotter(test_mesh, z, p_true, p_GP_opt):
-    # Plot Heat Map for SSE
+y_title = plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
 
+def basic_plotter(test_mesh, z, p_true, p_GP_opt):
+    '''
+    Plots heat maps for 2 input GP
+    '''
     #Defines the x and y coordinates that will be used to generate the heat map, this step isn't
     #necessary, but streamlines the process
     xx , yy = test_mesh
@@ -13,8 +16,8 @@ def y_plotter(test_mesh, z, p_true, p_GP_opt):
     plt.colorbar()
 
     #Plots the true optimal value and the GP value
-    plt.scatter(p_true[0],p_true[1], color="red", label = "True", s=50)
-    plt.scatter(p_GP_opt[0],p_GP_opt[1], color="orange", label = "GP")
+    plt.scatter(p_true[0],p_true[1], color="red", label = "True Optimal Value", s=50, marker = (5,1))
+    plt.scatter(p_GP_opt[0],p_GP_opt[1], color="orange", label = "GP Optimal Value", marker = ".")
 
     #Plots axes such that they are scaled the same way (eg. circles look like circles)
     plt.axis('scaled')
@@ -24,12 +27,14 @@ def y_plotter(test_mesh, z, p_true, p_GP_opt):
     plt.legend(loc = 'best')
 
     #Creates axis labels and title
-    plt.xlabel('Theta 1',weight='bold')
-    plt.ylabel('Theta 2',weight='bold')
+    plt.xlabel('$\\theta_1$',weight='bold')
+    plt.ylabel('$\\theta_2$',weight='bold')
     plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
 
     #Shows plot
     return plt.show()
+
+y_title = plt.title('Heat Map of SSE', weight='bold',fontsize = 16)
 
 def stdev_plotter(test_mesh, z, p_true, p_GP_opt):
 
