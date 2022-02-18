7acae5f87730342782373189ccd4f35bb642326d
Montana Carlozo <mcarlozo@nd.edu>
2022-02-18 12:35:24 -0500

    Changed comments

--- bo_functions.py
+++ bo_functions.py
@@ -21,17 +21,17 @@ def best_error_advanced(y_model, y_target):
     assert isinstance(y_target, np.ndarray)==True, "y_target must be an ndarray size 1xn"
     
     #Changes y_model type from torch tensor to numpy array
-    y_model = y_model.numpy()
+    y_model = y_model.numpy() #1xn
     
     #Asserst that that these y_model and y_target have equal lengths.
     assert len(y_target)==len(y_model), "y_target and y_model must be the same length"
     
     #Calculates best error as the maximum of the -error
-    error = (y_target-y_model)**2 #1x6
+    error = (y_target-y_model)**2 #1xn
     best_error = np.max(-error) #A number
     
     #Finds the best x index
-    best_x = np.argmax(-error) #1x3
+    best_x = np.argmax(-error) #1xlen(Z.T)
     
     #Makes best_error positive again
     best_error = -best_error
@@ -60,39 +60,39 @@ def calc_ei_advanced(f_best,pred_mean,pred_var,y_target):
     
     
     #Coverts tensor to np arrays
-    pred_mean = pred_mean.numpy()
-    pred_var = pred_var.numpy()
+    pred_mean = pred_mean.numpy() #1xn
+    pred_var = pred_var.numpy() #1xn
     
     #Checks for equal lengths
     assert len(y_target)==len(pred_mean)==len(pred_var), "y_target, pred_mean, and pred_var must be the same length"
     
     #Defines standard devaition
-    pred_stdev = np.sqrt(pred_var)
+    pred_stdev = np.sqrt(pred_var) #1xn
     
     #If variance is zero this is important
     with np.errstate(divide = 'warn'):
         #Creates upper and lower bounds and described by Nilay's word doc
-        bound_upper = ((y_target - pred_mean) +np.sqrt(f_best))/pred_var
-        bound_lower = ((y_target - pred_mean) -np.sqrt(f_best))/pred_var #(STDEV or VAR?)
+        bound_upper = ((y_target - pred_mean) +np.sqrt(f_best))/pred_var #1xn
+        bound_lower = ((y_target - pred_mean) -np.sqrt(f_best))/pred_var #(STDEV or VAR?) #1xn
 
         #Creates EI terms in terms of Nilay's code / word doc
-        ei_term1_comp1 = norm.pdf(bound_upper) - norm.pdf(bound_lower) #Why is this a CDF and not a PDF?
-        ei_term1_comp2 = f_best - (y_target - pred_mean)**2
+        ei_term1_comp1 = norm.pdf(bound_upper) - norm.pdf(bound_lower) #Why is this a CDF and not a PDF? #1xn
+        ei_term1_comp2 = f_best - (y_target - pred_mean)**2 #1xn
 
-        ei_term2_comp1 = 2*(y_target - pred_mean)*pred_stdev #(STDEV or VAR?)
-        ei_term2_comp2 = (norm.pdf(bound_upper) - norm.pdf(bound_lower)) #This gives a large negative number when tested
+        ei_term2_comp1 = 2*(y_target - pred_mean)*pred_stdev #(STDEV or VAR?) #1xn
+        ei_term2_comp2 = (norm.pdf(bound_upper) - norm.pdf(bound_lower)) #This gives a large negative number when tested #1xn
 
-        ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2)) #(CDF or PDF)
-        ei_term3_comp2 = -norm.pdf(bound_upper)*bound_upper
-        ei_term3_comp3 = (1/2)*norm.pdf(bound_lower/np.sqrt(2)) #(CDF or PDF)
-        ei_term3_comp4 = -norm.pdf(bound_lower)*bound_lower
+        ei_term3_comp1 = (1/2)*norm.pdf(bound_upper/np.sqrt(2)) #(CDF or PDF) #1xn
+        ei_term3_comp2 = -norm.pdf(bound_upper)*bound_upper #1xn
+        ei_term3_comp3 = (1/2)*norm.pdf(bound_lower/np.sqrt(2)) #(CDF or PDF) #1xn
+        ei_term3_comp4 = -norm.pdf(bound_lower)*bound_lower #1xn
 
-        ei_term3_psi_upper = ei_term3_comp1 + ei_term3_comp2
-        ei_term3_psi_lower = ei_term3_comp3 + ei_term3_comp4
+        ei_term3_psi_upper = ei_term3_comp1 + ei_term3_comp2 #1xn
+        ei_term3_psi_lower = ei_term3_comp3 + ei_term3_comp4 #1xn
 
-        ei_term1 = ei_term1_comp1*ei_term1_comp2
-        ei_term2 = ei_term2_comp1*ei_term2_comp2
-        ei_term3 = -pred_var*(ei_term3_psi_upper-ei_term3_psi_lower)
+        ei_term1 = ei_term1_comp1*ei_term1_comp2 #1xn
+        ei_term2 = ei_term2_comp1*ei_term2_comp2 #1xn
+        ei_term3 = -pred_var*(ei_term3_psi_upper-ei_term3_psi_lower) #1xn
 
-        ei = ei_term1 + ei_term2 + ei_term3
+        ei = ei_term1 + ei_term2 + ei_term3 #1xn
     return ei
