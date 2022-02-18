9f1bd5c5864aa82415ddd46d4a4ba29cda1e63d1
Montana Carlozo <mcarlozo@nd.edu>
2022-02-18 10:56:41 -0500

    Deleted calc ei_basic based on code review

--- bo_functions.py
+++ bo_functions.py
@@ -1,44 +1,6 @@
 import numpy as np
 from scipy.stats import norm
 
-def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias):
-    """ 
-    Calculates the expected improvement of the 2 input parameter GP
-    Parameters
-    ----------
-        f_best: float, the best predicted sse encountered
-        pred_mean: tensor, model mean
-        pred_var, tensor, model variance
-        explore_bias: float, the numerical bias towards exploration
-    
-    Returns
-    -------
-        ei: ndarray, the expected improvement of the GP model
-    
-    """
-    #Creates empty list to store ei values
-    ei = np.zeros(len(pred_var)) #1xn
-    
-    #Converts tensors to np arrays and defines standard deviation
-    pred_mean = pred_mean.numpy() #1xn
-    pred_var = pred_var.numpy()    #1xn
-    pred_stdev = np.sqrt(pred_var) #1xn
-    
-    #Loops over every standard deviation values
-    for i in range(len(pred_var)):
-        #Checks that all standard deviations are positive
-        if pred_stdev[i] > 0:
-            #Calculates ei based on Ke's formula
-            z = (pred_mean[i] - f_best - explore_bias)/pred_stdev[i] #number
-            ei_term_1 = (pred_mean[i] - f_best - explore_bias)*norm.cdf(z) #number
-            ei_term_2 = pred_stdev[i]*norm.pdf(z) #number
-            ei[i] = ei_term_1 +ei_term_2 #A number
-        else:
-            #Sets ei to zero if standard deviation is zero
-            ei[i] = 0
-    return ei
-
-
 def best_error_advanced(test_p, y_model, y_target):
     
     """
