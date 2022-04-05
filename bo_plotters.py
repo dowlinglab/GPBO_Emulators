8f82976eacf23457cc5c7b775236b88157cc422b
Montana Carlozo <mcarlozo@nd.edu>
2022-04-05 16:47:35 -0400

    Added testing plotters

--- bo_plotters.py
+++ bo_plotters.py
@@ -4,6 +4,65 @@ import torch
 from mpl_toolkits.mplot3d import Axes3D
 from pylab import *
 
+def plotter_adv_test(parameter_space, z,plot_title="Model Output"):
+    """
+    Plots the values of the GP given by the user
+    Parameters
+    ----------
+        parameter_space: tensor or ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
+        z:  tensor or ndarray, nx1 array of values
+        plot_title: str, The title for the graph
+    
+    Returns
+    -------
+        A 3D Heat map of the values of z predicted by the GP
+    """
+    #Converts tensors and tuples to ndarrays
+    if torch.is_tensor(parameter_space)==True:
+        parameter_space= parameter_space.numpy()
+        
+    if isinstance(z,ndarray)!=True:
+        z = np.asarray(z)
+
+        
+    #Asserts that the parameter space is 3 inuts, the data to be plotted is an array, and the plot title is a string
+    assert len(parameter_space.T) == 3, "The GP is a 3 input GP. Please include only 3 input parameters to plot."
+    assert isinstance(plot_title,str) == True, "Plot title must be a string."
+    
+    #Breaks Parameter space into separate componenets
+    p_1 = parameter_space[:,0] #Theta1 #1xn
+    p_2 = parameter_space[:,1] #Theta2 #1xn
+    p_3 = parameter_space[:,2] #x #1xn
+    print(z)
+
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.add_subplot(111,projection='3d')
+
+    xs = p_1
+    ys = p_2
+    zs = P_3
+
+    the_fourth_dimension = z
+
+    colors = cm.hsv(the_fourth_dimension/max(the_fourth_dimension))
+
+    colmap = cm.ScalarMappable(cmap=cm.hsv)
+    colmap.set_array(the_fourth_dimension)
+
+    yg = ax.scatter(xs, ys, zs, c=colors, marker='o')
+    cb = fig.colorbar(colmap)
+
+    # adding title and labels
+    ax.set_title("Heat Map of "+plot_title)
+    ax.set_xlabel('$\\theta_1$')
+    ax.set_ylabel('$\\theta_2$')
+    ax.set_zlabel('x coordinate')
+    
+    # displaying plot
+    plt.savefig(plot_title+'.png')
+    plt.show()
+    return 
+
 def plotter_adv(parameter_space, z,plot_title="Model Output"):
     """
     Plots the values of the GP given by the user
@@ -33,7 +92,7 @@ def plotter_adv(parameter_space, z,plot_title="Model Output"):
     p_1 = parameter_space[:,0] #Theta1 #1xn
     p_2 = parameter_space[:,1] #Theta2 #1xn
     p_3 = parameter_space[:,2] #x #1xn
-    
+    print(z)
     #Sets what data will be within the graph as the heat map points
     color = z
 
@@ -47,8 +106,8 @@ def plotter_adv(parameter_space, z,plot_title="Model Output"):
 #     print(color_map.set_array(color))
 
     # creating the heatmap
-    img = ax.scatter(p_1, p_2, p_3, marker='s',
-                     s=200, color='green')
+#     img = ax.scatter(p_1, p_2, p_3, marker='s', s=200, color='green')
+    img = ax.scatter(p_1, p_2, p_3, marker='s',s =200, color='green')
     plt.colorbar(color_map)
 
     # adding title and labels
@@ -156,3 +215,50 @@ def improvement_int_terms_plot(z, term_num, index_num):
         plt.show()
     return 
 
+
+#TEST PLOTTERS
+
+def y_plotter_adv_test(parameter_space, z,plot_title):
+    """
+    Plots the y values of the GP
+    Parameters
+    ----------
+        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
+        z:  ndarray, nx1 array of values the GP predicted function values
+        title: str, The title for the graph
+    
+    Returns
+    -------
+        A 3D Heat map of the values of z predicted by the GP
+    """
+    return plotter_adv_test(parameter_space, z,plot_title)
+
+def stdev_plotter_adv_test(parameter_space, z):
+    """
+    Plots the standard deviation alues of the GP
+    Parameters
+    ----------
+        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
+        z:  ndarray, nx1 array of the GP predicted standard deviation values
+    
+    Returns
+    -------
+        A 3D Heat map of the values of standard deviation predicted by the GP
+    """
+    title = "Standard Deviation"
+    return plotter_adv_test(parameter_space, z,title)
+
+def ei_plotter_adv_test(parameter_space, z):
+    """
+    Plots the expected improvement of the GP
+    Parameters
+    ----------
+        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
+        z:  ndarray, nx1 array of the GP expected improvement values
+    
+    Returns
+    -------
+        A 3D Heat map of the values of expected improvement predicted by the GP
+    """
+    title = "Expected Improvement"
+    return plotter_adv_test(parameter_space, z,title)
