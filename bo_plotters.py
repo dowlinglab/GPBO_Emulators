c4118291f63c164a144db97ca60cc5dab9273906
Montana Carlozo <mcarlozo@nd.edu>
2022-04-21 18:00:24 -0400

    Got rid of plotters that were unnecessary

--- bo_plotters.py
+++ bo_plotters.py
@@ -4,109 +4,6 @@ import torch
 from mpl_toolkits.mplot3d import Axes3D
 from pylab import *
 
-def plotter_adv_4D(parameter_space, z, point_num, plot_title="Model Output",yval = False):
-    """
-    Plots the values of the GP given by the user
-    Parameters
-    ----------
-        parameter_space: tensor or ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
-        z:  tensor or ndarray, nx1 array of values
-        plot_title: str, The title for the graph
-    
-    Returns
-    -------
-        A 4D Heat map of the values of z predicted by the GP
-    """
-    #Converts tensors and tuples to ndarrays
-    if torch.is_tensor(parameter_space)==True:
-        parameter_space= parameter_space.numpy()
-        
-    if isinstance(z,ndarray)!=True:
-        z = np.asarray(z)
-   
-    #Asserts that the parameter space is 3 inuts, the data to be plotted is an array, and the plot title is a string
-    assert isinstance(plot_title,str) == True, "Plot title must be a string."
-
-    #https://stackoverflow.com/questions/17756925/how-to-plot-heatmap-colors-in-3d-in-matplotlib
-    
-    # Define dimensions
-    X, Y, Z = parameter_space
-
-    # Create data
-    point_num = point_num
-    data = z.reshape(point_num,point_num,point_num).T
-
-    kw = {
-        'vmin': data.min(),
-        'vmax': data.max(),
-        'levels': np.linspace(data.min(), data.max()),
-    }
-
-    # Create a figure with 3D ax
-    fig = plt.figure(figsize=(5, 4))
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Plot contour surfaces
-    _ = ax.contourf(
-        X[:, :, -1], Y[:, :, -1], data[:, :, -1],
-        zdir='z', offset=Z.max(), **kw
-    ) 
-    _ = ax.contourf(
-        X[0, :, :], data[0, :, :], Z[0, :, :],
-        zdir='y', offset=Y.min(), **kw
-    )
-    C = ax.contourf(
-        data[:, -1, :], Y[:, -1, :], Z[:, -1, :],
-        zdir='x', offset=X.max(), **kw
-    )
-    #CHange these
-#     _ = ax.contourf(
-#     X[:, :, 0], Y[:, :, 0], data[:, :, 0],
-#     zdir='z', offset=Z.min(), **kw
-#     )
-#     _ = ax.contourf(
-#         X[-1, :, :], data[-1, :, :], Z[-1, :, :],
-#         zdir='y', offset=Y.max(), **kw
-#     )
-#     C = ax.contourf(
-#         data[:, 0, :], Y[:, 0, :], Z[:, 0, :],
-#         zdir='x', offset=X.min(), **kw
-#     )
-    # --
-
-
-    # Set limits of the plot from coord limits
-    xmin, xmax = X.min(), X.max()
-    ymin, ymax = Y.min(), Y.max()
-    zmin, zmax = Z.min(), Z.max()
-    ax.set(xlim=[xmin, xmax], ylim=[ymin, ymax], zlim=[zmin, zmax])
-
-    # Plot edges
-    edges_kw = dict(color='0.4', linewidth=1, zorder=1e3)
-    ax.plot([xmax, xmax], [ymin, ymax], [zmax, zmax], **edges_kw)
-    ax.plot([xmin, xmax], [ymin, ymin], [zmax, zmax], **edges_kw)
-    ax.plot([xmax, xmax], [ymin, ymin], [zmin, zmax], **edges_kw)
-
-    # Set labels and zticks
-    ax.set(
-        xlabel='$\Theta_1$',
-        ylabel='$\Theta_2$',
-        zlabel='x coord',
-    )
-    ax.set_title("Heat Map of "+plot_title, fontsize = 18)
-    # Set distance and angle view
-    ax.view_init(40, -30)
-    ax.dist = 11
-
-    # Colorbar
-    fig.colorbar(C, ax=ax, fraction=0.02, pad=0.1, label=plot_title)
-
-    # Show Figure
-    plt.savefig(plot_title+'_4D'+'.png')
-    plt.show()
-    
-    return 
-
 def plotter_adv(parameter_space, z,plot_title="Model Output",yval = False):
     """
     Plots the values of the GP given by the user
@@ -183,21 +80,6 @@ def y_plotter_adv(parameter_space, z,plot_title,yval):
     """
     return plotter_adv(parameter_space, z,plot_title,yval)
 
-def y_plotter_adv_4D(parameter_space, z,point_num,plot_title,yval):
-    """
-    Plots the y values of the GP
-    Parameters
-    ----------
-        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
-        z:  ndarray, nx1 array of values the GP predicted function values
-        title: str, The title for the graph
-        yval: True or False, will determine whether true values will be plotted with y model values
-    
-    Returns
-    -------
-        A 3D Heat map of the values of z predicted by the GP
-    """
-    return plotter_adv_4D(parameter_space, z,point_num,plot_title,yval)
 
 def stdev_plotter_adv(parameter_space, z):
     """
@@ -214,21 +96,6 @@ def stdev_plotter_adv(parameter_space, z):
     title = "Standard Deviation"
     return plotter_adv(parameter_space, z,title)
 
-def stdev_plotter_adv_4D(parameter_space, z,point_num):
-    """
-    Plots the standard deviation alues of the GP
-    Parameters
-    ----------
-        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
-        z:  ndarray, nx1 array of the GP predicted standard deviation values
-    
-    Returns
-    -------
-        A 3D Heat map of the values of standard deviation predicted by the GP
-    """
-    title = "Standard Deviation"
-    return plotter_adv_4D(parameter_space, z,point_num, title)
-
 def ei_plotter_adv(parameter_space, z):
     """
     Plots the expected improvement of the GP
@@ -243,22 +110,6 @@ def ei_plotter_adv(parameter_space, z):
     """
     title = "Expected Improvement"
     return plotter_adv(parameter_space, z, title)
-
-def ei_plotter_adv_4D(parameter_space, z, point_num):
-    """
-    Plots the expected improvement of the GP
-    Parameters
-    ----------
-        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
-        z:  ndarray, nx1 array of the GP expected improvement values
-        point_num: The amount of points of each input parameter used to generate the initial meshgrid
-    
-    Returns
-    -------
-        A 3D Heat map of the values of expected improvement predicted by the GP
-    """
-    title = "Expected Improvement"
-    return plotter_adv_4D(parameter_space, z, point_num, title)
 
 
 def error_plotter_adv(parameter_space, z, z2):
@@ -282,28 +133,6 @@ def error_plotter_adv(parameter_space, z, z2):
         
     error = (z2 - z)**2
     return plotter_adv(parameter_space, error,title)
-
-def error_plotter_adv_4D(parameter_space, z, z2, point_num):
-    """
-    Plots the expected improvement of the GP
-    Parameters
-    ----------
-        parameter_space: ndarray, meshgrid of 3 input parameters, Theta1, Theta2, and x
-        z:  ndarray, nx1 array of the GP expected improvement values
-    
-    Returns
-    -------
-        A 3D Heat map of the values of expected improvement predicted by the GP
-    """
-    title = "Error Magnitude"
-    
-    if isinstance(z,ndarray)!=True:
-        z = np.asarray(z)
-    if isinstance(z2,ndarray)!=True:
-        z2 = np.asarray(z2)
-        
-    error = np.sqrt((z2 - z)**2)
-    return plotter_adv_4D(parameter_space, error, point_num, title)
 
 def improvement_integral_plot(parameter_space, z):
     """
@@ -354,7 +183,7 @@ def improvement_int_terms_plot(z, term_num, index_num):
         plt.show()
     return 
 
-def basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train=False):
+def basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train=True):
     '''
     Plots heat maps for 2 input GP
     Parameters
@@ -425,7 +254,7 @@ def y_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,title="y",plot_train=
     '''
     return basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train)
 
-def stdev_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=False):
+def stdev_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=True):
     '''
     Helper function for basic_plotter. Calls basic_plotter specifically for plotting standard deviation values.
 
@@ -441,9 +270,9 @@ def stdev_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=False)
         plt.show(), A heat map of test_mesh and z (standard deviation values)
     '''
     title = "Standard Deviation"
-    return basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train=False)
+    return basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train)
 
-def ei_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=False):
+def ei_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=True):
     """
     Helper function for basic_plotter. Calls basic_plotter specifically for plotting expected improvement values.
 
@@ -459,7 +288,7 @@ def ei_plotter_basic(test_mesh, z, p_true, p_GP_opt,train_p,plot_train=False):
         plt.show(), A heat map of test_mesh and z (expected improvement values)
     """
     title = "Expected Improvement"
-    return basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train=False)
+    return basic_plotter(test_mesh, z, p_true, p_GP_opt,title,train_p,plot_train)
 
                         
     
