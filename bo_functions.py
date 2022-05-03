113405eee6fe589435e281c3202286ab8e82eecb
Montana Carlozo <mcarlozo@nd.edu>
2022-05-03 18:41:54 -0400

    Formatted 2-Input GP to use a for loop

--- bo_functions.py
+++ bo_functions.py
@@ -629,14 +629,16 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias=0.0):
     on the training data. The output will be a :obj:`~gpytorch.distributions.MultivariateNormal`.
     """
     assert len(pred_mean) == len(pred_var), "GP predicted means and variances must be the same length"
-    assert torch.is_tensor(pred_mean)==True and torch.is_tensor(pred_var)==True, "GP predicted means and variances must be tensors"
+#     assert torch.is_tensor(pred_mean)==True and torch.is_tensor(pred_var)==True, "GP predicted means and variances must be tensors"
     
     #Creates empty list to store ei values
     ei = np.zeros(len(pred_var)) #1xn_test
     
     #Converts tensors to np arrays and defines standard deviation
-    pred_mean = pred_mean.numpy() #1xn_test
-    pred_var = pred_var.detach().numpy()   #1xn_test
+    if torch.is_tensor(pred_mean)==True:
+        pred_mean = pred_mean.numpy() #1xn
+    if torch.is_tensor(pred_var)==True:
+        pred_var = pred_var.detach().numpy() #1xn
     pred_stdev = np.sqrt(pred_var) #1xn_test
 
     
@@ -655,4 +657,47 @@ def calc_ei_basic(f_best,pred_mean,pred_var, explore_bias=0.0):
         else:
             #Sets ei to zero if standard deviation is zero
             ei[i] = 0
+    return ei
+
+def calc_ei_basic2(f_best,pred_mean,pred_var, explore_bias=0.0):
+    """ 
+    Calculates the expected improvement of the 2 input parameter GP
+    Parameters
+    ----------
+        f_best: float, the best predicted sse encountered
+        pred_mean: tensor, model mean
+        pred_var, tensor, model variance
+        explore_bias: float, the numerical bias towards exploration, zero is the default
+    
+    Returns
+    -------
+    Calling this model will return the posterior of the latent Gaussian process when conditioned
+    on the training data. The output will be a :obj:`~gpytorch.distributions.MultivariateNormal`.
+    """
+#     assert len(pred_mean) == len(pred_var), "GP predicted means and variances must be the same length"
+#     assert torch.is_tensor(pred_mean)==True and torch.is_tensor(pred_var)==True, "GP predicted means and variances must be tensors"
+    
+    #Converts tensors to np arrays and defines standard deviation
+    if torch.is_tensor(pred_mean)==True:
+        pred_mean = pred_mean.numpy() #1xn
+    if torch.is_tensor(pred_var)==True:
+        pred_var = pred_var.detach().numpy() #1xn
+    pred_stdev = np.sqrt(pred_var) #1xn_test
+    print("stdev",pred_stdev)
+    #Checks that all standard deviations are positive
+    if pred_stdev > 0:
+        #Calculates z-score based on Ke's formula
+        z = (pred_mean - f_best - explore_bias)/pred_stdev #scaler
+        print("z",z)
+        #Calculates ei based on Ke's formula
+        #Explotation term
+        ei_term_1 = (pred_mean - f_best - explore_bias)*norm.cdf(z) #scaler
+        #Exploration Term
+        ei_term_2 = pred_stdev*norm.pdf(z) #scaler
+        ei = ei_term_1 +ei_term_2 #scaler
+        print("term 1",ei_term_1)
+        print("term 2",ei_term_2)
+    else:
+        #Sets ei to zero if standard deviation is zero
+        ei = 0
     return ei
