b76e64dc24d16705fc089b7367d0151c295fd9cf
Montana Carlozo <mcarlozo@nd.edu>
2022-07-01 17:28:38 -0400

    Ran 3-Input GP and added files to submit jobs with

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # required in inputfile for smp 4:
 
 #$ -pe smp 1
@@ -7,11 +6,11 @@
 #$ -r n
 #$ -M mcarlozo@nd.edu
 #$ -q long
-#$ -N GP_runs_15_100
+#$ -N GP2_runs_15_100
 
-conda activate Toy_Problem_env
+export PATH=/afs/crc.nd.edu/user/m/mcarlozo/.conda/envs/Toy_Problem_env/bin:$PATH
 
 set INPUT = "$1"
 
-python3 $INPUT 
+python3  2_Input_GPBO.py
 
