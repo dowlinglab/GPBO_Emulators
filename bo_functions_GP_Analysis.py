f3a5422b5862d381d40d619ee1ef531a0f834d4b
Montana Carlozo <mcarlozo@nd.edu>
2022-09-09 18:12:22 -0400

    Fixed SF Sensitivity Analysis to work w/ 2-Input

--- bo_functions_GP_Analysis.py
+++ bo_functions_GP_Analysis.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from bo_functions import find_train_doc_path, train_GP_model, calc_y_exp, create_sse_data, create_y_data, find_train_doc_path, ExactGPModel, train_GP_model 
 from bo_functions import calc_GP_outputs, test_train_split
+from bo_functions import gen_y_Theta_GP
 
 import matplotlib.pyplot as plt
 from bo_plotters import plot_org_train
@@ -126,7 +127,7 @@ def LOO_eval_GP(train_y, model, likelihood, verbose, set_lengthscale):
 def set_eval_point():
     "Set the Eval point used in LOO_Analysis based off number of dimensions"
     
-def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std, obj, run, sparse_grid, emulator, set_lengthscale, verbose = False,save_fig=False, runs = 1, DateTime=None, test_p = None, LOO = True, LSO = False):
+def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std, obj, run, emulator, set_lengthscale, verbose = False,save_fig=False, runs = 1, DateTime=None, test_p = None, LOO = True, LSO = False):
     """
     Performs BO iterations
     
@@ -134,20 +135,18 @@ def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std,
     -----------
         train_p: tensor or ndarray, The training parameter space data
         train_y: tensor or ndarray, The training y data
-        theta_mesh: ndarray (d, p x p), meshgrid of Theta1 and Theta2
         Theta_True: ndarray, The array containing the true values of Theta1 and Theta2
         train_iter: int, number of training iterations to run. Default is 300
         Xexp: ndarray, The list of xs that will be used to generate y
         Yexp: ndarray, The experimental data for y (the true value)
         noise_std: float, int: The standard deviation of the noise
         obj: str, Must be either obj or LN_obj. Determines whether objective fxn is sse or ln(sse)
-        run: int, The iteration of the number of times new training points have been picked
-        sparse_grid: True/False: Determines whether a sparse grid or approximation is used for the GP emulator
+        run: int, The number of which run you are on
         emulator: True/False, Determines if GP will model the function or the function error
         set_lengthscale: float or None, Value of the lengthscale hyperparameter - None if hyperparameters will be updated during training
         verbose: True/False, Determines whether z_term, ei_term_1, ei_term_2, CDF, and PDF terms are saved, Default = False
         save_fig: True/False, Determines whether figures will be saved
-        runs: int, # of total runs
+        runs: int, the total number of runs
         DateTime: None or bool, Determines whether the date and time will be used to save figures
         test_p: ndarray, test points for GP
         LOO: bool, Determines whether leave one out is a true LOO plot or has random values that are left out
@@ -191,6 +190,118 @@ def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std,
     eval_components = LOO_eval_GP(train_y, model, likelihood, verbose, set_lengthscale)
 
     #Create LOO Plots
+    if emulator == True:
+        Theta, SSE_GP_Analy = LOO_Plots_3_Input(model, likelihood, Xexp, noise_std, run, verbose = verbose, save_fig=False, runs = runs, DateTime = None, test_p = test_p, LOO = LOO, LSO = LSO)
+        
+    if emulator == False:
+        Theta, SSE_GP_Analy = LOO_Plots_2_Input(model, likelihood, Xexp, noise_std, run, obj, verbose = verbose, save_fig=False, runs = 1, DateTime=None, test_p = test_p, LOO = LOO, LSO = LSO)
+
+    return Theta, SSE_GP_Analy
+
+def LOO_Plots_2_Input(model, likelihood, Xexp, noise_std, run, obj, verbose = False, save_fig=False, runs = 1, DateTime=None, test_p = None, LOO = True, LSO = False):
+    """
+     Creates LOO Plots for 3-Input - Note: SSE Calculation is identical for all 3-Input Methods (Sparse, Approx, log(EI) Approx)
+    
+    Parameters:
+    -----------
+        Xexp: ndarray, The list of xs that will be used to generate y
+        noise_std: float, int: The standard deviation of the noise
+        verbose: True/False, Determines whether z_term, ei_term_1, ei_term_2, CDF, and PDF terms are saved, Default = False
+        run: int, The number of which run you are on
+        obj: str, Must be either obj or LN_obj. Determines whether objective fxn is sse or ln(sse)
+        save_fig: True/False, Determines whether figures will be saved
+        DateTime: None or bool, Determines whether the date and time will be used to save figures
+        runs: int, the total number of runs
+        test_p: ndarray, test points for GP
+        LOO: bool, Determines whether leave one out is a true LOO plot or has random values that are left out
+        LSO: bool, determines whether 1 set of training points or multiple training points are left out
+   
+        
+    Returns:
+    --------
+        Theta: ndarray, tested theta value
+        SSE_GP_Analy: float, The predicted SSE
+    """
+    #Create LOO Plots
+    X_space = np.linspace(-2,2,50)
+    y_sim = np.zeros(len(X_space))
+        
+    if LOO == True and LSO == False:
+        point = [test_p[0,0],test_p[0,1]]
+        calc_eval_point = np.array([test_p[0,0],test_p[0,1]])
+    else:
+        point = [1.0, -1]
+        calc_eval_point = np.array([1,-1])
+
+    eval_point = np.array([point])
+#     print(eval_point)
+    GP_mean = calc_GP_outputs(model, likelihood, eval_point[0:1])[3]
+    GP_stdev = calc_GP_outputs(model, likelihood, eval_point[0:1])[1]
+
+    if obj == "obj":
+        SSE_GP_Analy = GP_mean
+    else:
+        SSE_GP_Analy = np.exp(GP_mean)
+
+    if LOO == True and LSO == False:
+        point = [test_p[0,0],test_p[0,1]]
+        calc_eval_point = np.array([test_p[0,0],test_p[0,1]])
+    else:
+        point = [1, -1]
+        calc_eval_point = np.array([1,-1]) 
+
+    eval_point = np.array([point])
+#             test_y[k] = calc_GP_outputs(model, likelihood, eval_point[0:1])[3]
+    
+    for k in range(len(X_space)):
+        if LOO == True and LSO == False:
+            point = [test_p[0,0],test_p[0,1],X_space[k]]
+            calc_eval_point = np.array([test_p[0,0],test_p[0,1]])
+        else:
+            point = [1, -1, X_space[k]]
+            calc_eval_point = np.array([1,-1])
+        
+        sim_point = np.array([point])
+        y_sim[k] = create_y_data(sim_point[0])
+    
+    test_y = calc_y_exp(calc_eval_point, Xexp, noise_std, noise_mean=0,random_seed=6)
+        
+    Theta = np.array([point[0:2]])[0]
+#     y_mean = gen_y_Theta_GP(X_exp, Theta)
+    
+    #How to do GP Analysis w/ 2-Input?
+#     if verbose == True:
+#         print("Showing X/Y Plot for Theta = ",Theta , "with Xexp =", Xexp)
+#         print("SSE_Total is", SSE_GP_Analy)
+         
+#         plot_2GP_performance(X_space, y_sim, GP_mean, GP_stdev, Theta, Xexp, test_p = test_p, test_y = test_y, verbose=verbose)
+#     print(Theta)    
+    return Theta, SSE_GP_Analy
+
+def LOO_Plots_3_Input(model, likelihood, Xexp, noise_std, run, verbose = False, save_fig=False, runs = 1, DateTime=None, test_p = None, LOO = True, LSO = False):
+    """
+     Creates LOO Plots for 3-Input - Note: SSE Calculation is identical for all 3-Input Methods (Sparse, Approx, log(EI) Approx)
+    
+    Parameters:
+    -----------
+        Xexp: ndarray, The list of xs that will be used to generate y
+        noise_std: float, int: The standard deviation of the noise
+        verbose: True/False, Determines whether z_term, ei_term_1, ei_term_2, CDF, and PDF terms are saved, Default = False
+        run: int, The number of which run you are on
+        save_fig: True/False, Determines whether figures will be saved
+        DateTime: None or bool, Determines whether the date and time will be used to save figures
+        runs: int, the total number of runs
+        test_p: ndarray, test points for GP
+        LOO: bool, Determines whether leave one out is a true LOO plot or has random values that are left out
+        LSO: bool, determines whether 1 set of training points or multiple training points are left out
+   
+        
+    Returns:
+    --------
+        Theta: ndarray, tested theta value
+        SSE_GP_Analy: float, The predicted SSE
+    """
+    #Create LOO Plots
     X_space = np.linspace(-2,2,50)
     y_sim = np.zeros(len(X_space))
     GP_mean = np.zeros(len(X_space))
@@ -206,9 +317,9 @@ def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std,
         else:
             point = [1, -1, X_space[k]]
             calc_eval_point = np.array([1,-1])
-            
-        eval_point = np.array([point])
         
+        eval_point = np.array([point])
+#         print(eval_point)
         y_sim[k] = create_y_data(eval_point[0])
         GP_mean[k] = calc_GP_outputs(model, likelihood, eval_point[0:1])[3]
         GP_stdev[k] = calc_GP_outputs(model, likelihood, eval_point[0:1])[1]
@@ -228,15 +339,16 @@ def LOO_Analysis(train_p,train_y, Theta_True, train_iter, Xexp, Yexp, noise_std,
         test_y = calc_y_exp(calc_eval_point, Xexp, noise_std, noise_mean=0,random_seed=6)
 
     Theta = np.array([point[0:2]])[0]
+#     print(Theta)
     if verbose == True:
         print("Showing X/Y Plot for Theta = ",Theta , "with Xexp =", Xexp)
         print("SSE_Total is", SSE_GP_Analy)
          
         plot_3GP_performance(X_space, y_sim, GP_mean, GP_stdev, Theta, Xexp, test_p = test_p, test_y = test_y, verbose=verbose)
-
+        
     return Theta, SSE_GP_Analy
 
-def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter,explore_bias, Xexp, Yexp, noise_std, obj, sparse_grid, emulator,set_lengthscale, len_data, verbose = False, save_fig = False, shuffle_seed = None, DateTime=None, LOO = True, LSO = False):
+def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter, Xexp, Yexp, noise_std, obj, emulator,set_lengthscale, len_data, verbose = False, save_fig = False, shuffle_seed = None, DateTime=None, LOO = True, LSO = False):
     """
     Performs BO iterations with runs. A run contains of choosing different initial training data.
     
@@ -245,12 +357,10 @@ def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter,explore_bias, Xexp, Yexp, 
         theta_mesh: ndarray (d, p x p), meshgrid of Theta1 and Theta2
         Theta_True: ndarray, The array containing the true values of Theta1 and Theta2
         train_iter: int, number of training iterations to run. Default is 300
-        explore_bias: float,int,tensor,ndarray (1 value) The exploration bias parameter
         Xexp: ndarray, The list of xs that will be used to generate y
         Yexp: ndarray, The experimental data for y (the true value)
         noise_std: float, int: The standard deviation of the noise
         obj: str, Must be either obj or LN_obj. Determines whether objective fxn is sse or ln(sse)
-        sparse_grid: Determines whether a sparse grid or approximation is used for the GP emulator
         emulator: True/False, Determines if GP will model the function or the function error
         set_lengthscale: float or None, Value of the lengthscale hyperparameter - None if hyperparameters will be updated during training
         len_data: int, original number of data points in the document
@@ -280,8 +390,8 @@ def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter,explore_bias, Xexp, Yexp, 
     p = theta_mesh.shape[1] #Number of training points to evaluate in each dimension of q
     n = len(Xexp)
     BO_iters = 1
-#     sep_facts = np.linspace(0.05,0.95,19)
-    sep_facts = [0.95, 0.8, 0.7, 0.6, 0.5]
+    sep_facts = np.linspace(0.05,0.95,19)
+#     sep_facts = [0.95, 0.8, 0.7, 0.6, 0.5]
     ln_SSE_GP_Analy_List = []
     dim = m+q #dimensions in a CSV
     
@@ -289,6 +399,7 @@ def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter,explore_bias, Xexp, Yexp, 
     
     all_data_doc = find_train_doc_path(emulator, obj, len_data)
     all_data = np.array(pd.read_csv(all_data_doc, header=0,sep=","))   
+#     print(all_data)
     
     if LOO==True and LSO == False:
         runs = 20
@@ -321,14 +432,15 @@ def LSO_LOO_Analysis(theta_mesh,Theta_True,train_iter,explore_bias, Xexp, Yexp, 
             test_p = test_data[:,1:(q+1)]
             assert len(train_p.T) ==q, "train_p must have the same number of dimensions as the value of q"
             
+#         print(test_p)
         train_y = train_data[:,-1]
         assert len(train_p) == len(train_y), "Training data must be the same length"
                           
 #         plot_org_train(theta_mesh,train_p,Theta_True)
-        plot_org_train(theta_mesh,train_p, test_p, Theta_True, emulator, sparse_grid, obj, explore_bias, set_lengthscale, i, save_fig, BO_iters, runs, DateTime, verbose)
+        plot_org_train(theta_mesh,train_p, test_p, Theta_True, emulator, None, obj, None, set_lengthscale, i, save_fig, BO_iters, runs, DateTime, verbose)
 
         #Run BO iteration
-        Theta, SSE_for_sep_fact = LOO_Analysis(train_p, train_y, Theta_True, train_iter, Xexp, Yexp, noise_std, obj, i, sparse_grid, emulator, set_lengthscale, verbose, save_fig, runs, DateTime, test_p, LOO = LOO, LSO = LSO)
+        Theta, SSE_for_sep_fact = LOO_Analysis(train_p, train_y, Theta_True, train_iter, Xexp, Yexp, noise_std, obj, i, emulator, set_lengthscale, verbose, save_fig, runs, DateTime, test_p, LOO = LOO, LSO = LSO)
         ln_SSE_GP_Analy_List.append(np.log(SSE_for_sep_fact))
         
     if LSO==True:
