d1627eb5d6f87b2d8aaed73c86204cd80fc1a5f9
Montana Carlozo <mcarlozo@nd.edu>
2022-11-16 10:12:10 -0500

    Fixed sse total bug for 3 input and saved all CSV data and figures to 2022 for GP Analysis

--- GP_Validation.py
+++ GP_Validation.py
@@ -15,21 +15,23 @@ from scipy.stats import qmc
 from sklearn.model_selection import LeaveOneOut
 
 from bo_functions_generic import train_GP_model, ExactGPModel, find_train_doc_path, clean_1D_arrays, set_ep, calc_GP_outputs
+from CS2_bo_plotters import save_csv, save_fig
 from CS2_create_data import gen_y_Theta_GP, calc_y_exp, create_y_data, create_sse_data
 # from CS1_create_data import gen_y_Theta_GP, calc_y_exp, create_y_data, create_sse_data, create_sse_data_GP_val
 ###Load data
 ###Get constants
 ##Note: X and Y should be 400 points long generated from meshgrid values and calc_y_exp :)
-def LOO_Analysis(all_data, ep, Xexp, Yexp, true_model_coefficients, true_p, emulator, obj, skip_param_types = 0, set_lengthscale = None, train_iter = 300, noise_std = 0.1, verbose = False):
-    ep_init = ep
+def LOO_Analysis(all_data, Xexp, Yexp, true_model_coefficients, true_p, emulator, obj, Case_Study, skip_param_types = 0, set_lengthscale = None, train_iter = 300, noise_std = 0.1, verbose = False, DateTime = None, save_figure= True):
     m = Xexp.shape[1]
     q = true_p.shape[0]
+    t = len(all_data)
 #     print(m)
     loo = LeaveOneOut()
     loo.get_n_splits(all_data)
     
     index_list = []
     model_list = []
+    y_sim_list = []
     sse_model_list = []
     #Loop over all test indecies & #Shuffle and split into training and testing data where 1 point is testing data
     for train_index, test_index in loo.split(all_data):
@@ -57,13 +59,12 @@ def LOO_Analysis(all_data, ep, Xexp, Yexp, true_model_coefficients, true_p, emul
         #Train GP
         train_GP = train_GP_model(model, likelihood, train_p, train_y, train_iter, verbose=verbose)
         #Set exploration parameter (in thic case, 1)
-        explore_bias = ep_init #Sets ep to the multiplicative scaler between 0.1 and 1
         #Evaluate GP on test set
         #Theta_set will be be only the test value
 #         print(test_p)
         test_p_reshape = test_p.numpy()
 #         print(test_p_reshape, test_p_reshape.shape)
-        eval_components = LOO_eval_GP(test_p_reshape, Xexp, train_y, explore_bias, true_model_coefficients, model, likelihood, verbose, emulator, set_lengthscale, train_p = train_p, obj = obj, skip_param_types = skip_param_types, noise_std = noise_std)
+        eval_components = LOO_eval_GP(test_p_reshape, Xexp, train_y, true_model_coefficients, model, likelihood, verbose, emulator, set_lengthscale, train_p = train_p, obj = obj, skip_param_types = skip_param_types, noise_std = noise_std)
 #         print("Evaluated")
         if emulator == False:
             GP_mean,GP_var,GP_stdev = eval_components
@@ -72,6 +73,7 @@ def LOO_Analysis(all_data, ep, Xexp, Yexp, true_model_coefficients, true_p, emul
             GP_mean,GP_var,GP_stdev, sse, sse_GP_var, sse_GP_stdev  = eval_components
 #             print(sse.shape, sse)
             sse_model_list.append(sse)
+            y_sim_list.append(data_test[:,-1])
             
         if test_index%50 ==0:
             print("Loop")
@@ -81,26 +83,25 @@ def LOO_Analysis(all_data, ep, Xexp, Yexp, true_model_coefficients, true_p, emul
     index_list = np.array(index_list)
     model_list = np.array(model_list)
     sse_model_list = np.array(sse_model_list)
+    
     if emulator == False:
         if obj == "LN_obj":
             sse_sim = np.exp(all_data[:,-1])
         else:
             sse_sim = all_data[:,-1]
-        LOO_Plots_2_Input(index_list, model_list, sse_sim, GP_stdev, true_p)  
-    else:
-#         all_theta = all_data[:,1:-m-1]
-#         print(all_theta.shape)
-#         X_sim = all_data[:,-m-1:-1]
-#         Y_exp = calc_y_exp(true_p, X_sim, noise_std)
-#         sse_data = create_sse_data_GP_val(q,all_theta, X_sim, Y_exp, obj = "obj")  #Not working correctly
-        LOO_Plots_3_Input(index_list, all_data[:,-1], model_list, GP_stdev, true_p)
-        print("SSE Total = ", sum( (all_data[:,-1] - model_list)**2 ) )
-#         LOO_Plots_2_Input(index_list, sse_model_list, sse_data, GP_stdev, true_p)
-        #Calculate SSE (maybe)
-        #Make residual plots (maybe)
+        LOO_Plots_2_Input(index_list, model_list, sse_sim, GP_stdev, true_p, Case_Study, DateTime, obj, set_lengthscale, save_figure)
+        
+    else:        
+        LOO_Plots_3_Input(index_list, model_list, all_data[:,-1], GP_stdev, true_p, Case_Study, DateTime, set_lengthscale, save_figure)
+        y_sim_list = np.array(y_sim_list)
+        fxn = "LOO_Plots_3_Input"
+        SSE_Total =  sum( (y_sim_list - model_list)**2 ) 
+        sse_tot_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/sse_tot")
+#         print(sse_tot_path)
+        print("SSE Total = ",'{:.4e}'.format(SSE_Total) )
     return
 
-def LOO_eval_GP(theta_set, Xexp, train_y, explore_bias, true_model_coefficients, model, likelihood, verbose, emulator, set_lengthscale, train_p = None, obj = "obj", skip_param_types = 0, noise_std = 0.1):
+def LOO_eval_GP(theta_set, Xexp, train_y, true_model_coefficients, model, likelihood, verbose, emulator, set_lengthscale, train_p = None, obj = "obj", skip_param_types = 0, noise_std = 0.1):
     """
     Evaluates GP
     
@@ -153,14 +154,14 @@ def LOO_eval_GP(theta_set, Xexp, train_y, explore_bias, true_model_coefficients,
     
     #Evaluate GP based on error emulator or property emulator
     if emulator == False:
-        eval_components = LOO_eval_GP_basic_set(theta_set, train_y, model, likelihood, obj, explore_bias, verbose)
+        eval_components = LOO_eval_GP_basic_set(theta_set, train_y, model, likelihood, obj, verbose)
     else:
 #         eval_components = eval_GP_emulator_tot(Xexp,Yexp, theta_mesh, model, likelihood, sparse_grid, explore_bias, verbose)
-        eval_components = LOO_eval_GP_emulator_set(theta_set, Xexp, true_model_coefficients, model, likelihood, explore_bias, verbose, train_p, obj, skip_param_types = skip_param_types,  noise_std = noise_std)
+        eval_components = LOO_eval_GP_emulator_set(theta_set, Xexp, true_model_coefficients, model, likelihood, verbose, train_p, obj, skip_param_types = skip_param_types,  noise_std = noise_std)
     
     return eval_components
 
-def LOO_eval_GP_basic_set(theta_set, train_sse, model, likelihood, obj = "obj", explore_bias=0.0, verbose = False):
+def LOO_eval_GP_basic_set(theta_set, train_sse, model, likelihood, obj = "obj", verbose = False):
     """ 
     Calculates the expected improvement of the 2 input parameter GP
     Parameters
@@ -229,7 +230,7 @@ def LOO_eval_GP_basic_set(theta_set, train_sse, model, likelihood, obj = "obj", 
 #     print(sse)
     return sse, var, stdev #Prints just the value
     
-def LOO_eval_GP_emulator_set(theta_set, Xexp, true_model_coefficients, model, likelihood, explore_bias = 0.0, verbose = False, train_p = None, obj = "obj", skip_param_types = 0, noise_std = 0.1):
+def LOO_eval_GP_emulator_set(theta_set, Xexp, true_model_coefficients, model, likelihood, verbose = False, train_p = None, obj = "obj", skip_param_types = 0, noise_std = 0.1):
     """ 
     Calculates the expected improvement of the 3 input parameter GP
     Parameters
@@ -305,8 +306,11 @@ def LOO_eval_GP_emulator_set(theta_set, Xexp, true_model_coefficients, model, li
 #     print(GP_mean_all)
     return GP_mean_all, GP_var_all, GP_stdev, SSE, SSE_var_GP, SSE_stdev_GP
 
-def LOO_Plots_2_Input(iter_space, GP_mean, sse_sim, GP_stdev, Theta):
+def LOO_Plots_2_Input(iter_space, GP_mean, sse_sim, GP_stdev, Theta, Case_Study, DateTime, obj, set_lengthscale = None, save_figure= True):
     p = GP_mean.shape[0]
+    emulator = False
+    fxn = "LOO_Plots_2_Input"
+    t = len(iter_space)
 #     print(X1.shape, X2.shape, GP_mean.shape)
     # Compare the experiments to the true model
     
@@ -340,17 +344,42 @@ def LOO_Plots_2_Input(iter_space, GP_mean, sse_sim, GP_stdev, Theta):
 #     plt.gca().axes.yaxis.set_ticklabels([])
 #     plt.title("BO Iteration Results: Lowest Overall ln(SSE)")
 #     plt.grid(True)
-    return plt.show()
 
-def LOO_Plots_3_Input(iter_space, Y_space, GP_mean, GP_stdev, Theta):
+    #Save CSVs
+    iter_space_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/iter_space")
+    GP_mean_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/log_sse_model")
+    sse_sim_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/log_sse_sim")
+    csv_item_list = [iter_space, np.log(GP_mean), np.log(sse_sim)]
+    make_csv_list = [iter_space_path, GP_mean_path, sse_sim_path]
+    
+    for i in range(len(make_csv_list)):
+        save_csv(csv_item_list[i], make_csv_list[i], ext = "npy")
+#         print("2", make_csv_list[i])
+    
+    #Save Figures (if applicable)
+    if save_figure == True:
+        path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = True)
+        save_fig(path, ext='png', close=True, verbose=False) 
+#         print(path)
+    else:
+        plt.show()
+        plt.close()
+        
+    return
+
+def LOO_Plots_3_Input(iter_space, GP_mean, y_sim, GP_stdev, Theta, Case_Study, DateTime, set_lengthscale = None, save_figure = True):
+    fxn = "LOO_Plots_3_Input"
+    emulator = True
     p = GP_mean.shape[0]
+    t = len(iter_space)
+    obj = "obj"
 #     print(X1.shape, X2.shape, GP_mean.shape)
     # Compare the experiments to the true model
     
     #Plot Minimum SSE value at each run
     plt.figure(figsize = (6.4,4))
     plt.scatter(iter_space,GP_mean, label = "$y_{model}$", s=100 )
-    plt.scatter(iter_space,Y_space, label = "$y_{sim}$" , s=50)
+    plt.scatter(iter_space,y_sim, label = "$y_{sim}$" , s=50)
     
 #     ax.fill_between(iter_space,
 #     GP_mean - 1.96 * GP_stdev,
@@ -375,4 +404,88 @@ def LOO_Plots_3_Input(iter_space, Y_space, GP_mean, GP_stdev, Theta):
 #     plt.gca().axes.yaxis.set_ticklabels([])
 #     plt.title("BO Iteration Results: Lowest Overall ln(SSE)")
 #     plt.grid(True)
-    return plt.show()
+    #Save CSVs
+    iter_space_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/iter_space")
+    GP_mean_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/y_model")
+    y_sim_path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = False, csv_end = "/y_sim")
+    csv_item_list = [iter_space, GP_mean, y_sim]
+    make_csv_list = [iter_space_path, GP_mean_path, y_sim_path]
+    
+    for i in range(len(make_csv_list)):
+        save_csv(csv_item_list[i], make_csv_list[i], ext = "npy")
+#         print("3", make_csv_list[i])
+    
+    #Save Figures (if applicable)
+    if save_figure == True:
+        path = path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime, is_figure = True)
+#         print(path)
+        save_fig(path, ext='png', close=True, verbose=False) 
+    else:
+        plt.show()
+        plt.close()
+        
+    
+    return
+
+def path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime = None, is_figure = True, csv_end = None):
+    """
+    names a path
+    
+    Parameters
+    ----------
+        emulator: True/False, Determines if GP will model the function or the function error
+        ep: float, float,int,tensor,ndarray (1 value) The original exploration bias parameter
+        sparse_grid: True/False, True/False: Determines whether a sparse grid or approximation is used for the GP emulator
+        fxn: str, The name of the function whose file path name will be created
+        set_lengthscale: float or None, The value of the lengthscale hyperparameter or None if hyperparameters will be updated at training
+        t: int, int, Number of initial training points to use
+        obj: str, Must be either obj or LN_obj. Determines whether objective fxn is sse or ln(sse)
+        mesh_combo: str, the name of the combination of parameters - Used to make a folder name
+        bo_iter: int, integer, number of the specific BO iterations
+        title_save: str or None,  A string containing the title of the file of the plot
+        run, int or None, The iteration of the number of times new training points have been picked
+        tot_iter: int, The total number of iterations. Printed at top of job script
+        tot_runs: int, The total number of times training data/ testing data is reshuffled. Printed at top of job script
+        DateTime: str or None, Determines whether files will be saved with the date and time for the run, Default None
+        sep_fact: float, Between 0 and 1. Determines fraction of all data that will be used to train the GP. Default is 1.
+        is_figure: bool, used for saving CSVs as part of this function and for calling the data from a CSV to make a plot
+        csv_end: str, the name of the csv file
+    Returns:
+        path: str, The path to which the file is saved
+    
+    """
+
+    obj_str = "/"+str(obj)
+    len_scl = "/len_scl_varies"
+    org_TP_str = "/TP_"+ str(t)
+    CS = "/CS_" + str(Case_Study)
+
+    if emulator == False:
+        Emulator = "/GP_Error_Emulator"
+        method = ""
+    else:
+        Emulator = "/GP_Emulator"
+            
+    fxn_dict = {"LOO_Plots_2_Input":"/SSE_gp_val" , "LOO_Plots_3_Input":"/y_gp_val"}
+    plot = fxn_dict[fxn]        
+      
+    if DateTime is not None:
+#         path_org = "../"+DateTime #Will send to the Datetime folder outside of CS1
+        path_org = DateTime #Will send to the Datetime folder outside of CS1
+    else:
+        path_org = "Test_Figs"
+        
+#         path_org = "Test_Figs"+"/Sep_Analysis2"+"/Figures"
+    if is_figure == True:
+        path_org = path_org + "/Figures"
+    else:
+        path_org = path_org + "/CSV_Data"
+        
+    path_end = CS + Emulator + org_TP_str + obj_str + len_scl + plot     
+
+    path = path_org + "/GP_Validation_Figs" + path_end 
+        
+    if csv_end is not None:
+        path = path + csv_end
+#     print(path)   
+    return path
