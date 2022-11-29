d79d1d3cf4ad7db1881793c37426d37bf2785f34
Montana Carlozo <mcarlozo@nd.edu>
2022-11-29 13:14:01 -0500

    2.2 GP val job  finished, ran bo_functions BO to show error in saving variance figure a1-b1 for method1A run 1 iter 1

--- CS2_bo_functions_multi_dim.py
+++ CS2_bo_functions_multi_dim.py
@@ -16,8 +16,8 @@ import time
 import Tasmanian
 
 #Notes: Change line below when changing test problems
-# from CS2_create_data import calc_muller, create_sse_data, create_y_data, calc_y_exp, gen_y_Theta_GP, eval_GP_emulator_BE, make_next_point
-from CS1_create_data import create_sse_data, create_y_data, calc_y_exp, gen_y_Theta_GP, eval_GP_emulator_BE, make_next_point
+from CS2_create_data import calc_muller, create_sse_data, create_y_data, calc_y_exp, gen_y_Theta_GP, eval_GP_emulator_BE, make_next_point
+# from CS1_create_data import create_sse_data, create_y_data, calc_y_exp, gen_y_Theta_GP, eval_GP_emulator_BE, make_next_point
 
 # from bo_functions_generic import LHS_Design, calc_y_exp, calc_muller, create_sse_data, create_y_data, set_ep, gen_y_Theta_GP, test_train_split, find_train_doc_path, ExactGPModel, train_GP_model, calc_GP_outputs, explore_parameter, ei_approx_ln_term, calc_ei_emulator, eval_GP_emulator_BE, get_sparse_grids, eval_GP_sparse_grid, calc_ei_basic, train_test_plot_preparation, clean_1D_arrays
 
