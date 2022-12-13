Fixed bugs related to normalization in CS2 make_y_data and added case_study argument to bo functions

bo_methods_lib/CS2_bo_functions_multi_dim.py
@@ -854,7 +854,7 @@ def eval_GP(theta_set, train_y, explore_bias, Xexp, Yexp, true_model_coefficient
     
     return eval_components
 
-def bo_iter(BO_iters,train_p,train_y,theta_set,Theta_True,train_iter,explore_bias, Xexp, Yexp, noise_std, obj, run, sparse_grid, emulator, set_lengthscale, true_model_coefficients, param_dict, verbose = False,save_fig=False, tot_runs = 1, DateTime=None, test_p = None, sep_fact = 1, LHS = False, skip_param_types = 0, eval_all_pairs = False, normalize = False, norm_scalers = None):
+def bo_iter(BO_iters,train_p,train_y,theta_set,Theta_True,train_iter,explore_bias, Xexp, Yexp, noise_std, obj, run, sparse_grid, emulator, set_lengthscale, true_model_coefficients, param_dict, verbose = False,save_fig=False, tot_runs = 1, DateTime=None, test_p = None, sep_fact = 1, LHS = False, skip_param_types = 0, eval_all_pairs = False, normalize = False, norm_scalers = None, case_study = 1):
     """
     Performs BO iterations
     
@@ -1057,7 +1057,10 @@ def bo_iter(BO_iters,train_p,train_y,theta_set,Theta_True,train_iter,explore_bia
                 print("Scipy Theta Opt = ",theta_o_unscl)
         
         #Calculate values of y given the GP optimal theta values
-        y_GP_Opt = gen_y_Theta_GP(Xexp, theta_o, true_model_coefficients, skip_param_types, norm_scalers, emulator)
+        if case_study == 1:
+            y_GP_Opt = gen_y_Theta_GP(Xexp, theta_o, true_model_coefficients, skip_param_types, norm_scalers, emulator)
+        else:
+            y_GP_Opt = gen_y_Theta_GP(Xexp, theta_o, true_model_coefficients, Theta_True, case_study, skip_param_types, norm_scalers, emulator)
         
         #Calculate GP SSE and save value
         ln_error_mag = np.log(np.sum((y_GP_Opt-Yexp)**2)) #Should SSE be calculated like this or should we use the GP approximation?
@@ -1253,8 +1256,8 @@ def bo_iter_w_runs(BO_iters,all_data_doc,t,theta_set,Theta_True,train_iter,explo
             assert len(train_p.T) ==q, "train_p must have the same number of dimensions as the value of q"
         
         #Plot all training data
-        #This works, put it back when we need it (12/7/22)
-        train_test_plot_preparation(q, m, theta_set, train_p, test_p, Theta_True, Xexp, emulator, sparse_grid, obj, ep0, set_lengthscale, i, save_fig, BO_iters, runs, DateTime, verbose, param_dict, sep_fact, normalize)
+        #This works, put it back when we need it (12/13/22)
+#         train_test_plot_preparation(q, m, theta_set, train_p, test_p, Theta_True, Xexp, emulator, sparse_grid, obj, ep0, set_lengthscale, i, save_fig, BO_iters, runs, DateTime, verbose, param_dict, sep_fact, normalize)
 
         #Normalize data as appropriate
         if normalize == True:
@@ -1265,9 +1268,9 @@ def bo_iter_w_runs(BO_iters,all_data_doc,t,theta_set,Theta_True,train_iter,explo
 #             print(test_p_scl)
             
             #Run BO Iteration
-            BO_results = bo_iter(BO_iters,train_p_scl,train_y,theta_set_scl,Theta_True_scl,train_iter,explore_bias, Xexp_scl, Yexp, noise_std, obj, i, sparse_grid, emulator, set_lengthscale, true_model_coefficients_scl, param_dict, verbose, save_fig, runs, DateTime, test_p_scl, sep_fact = sep_fact, LHS = LHS, skip_param_types = skip_param_types, eval_all_pairs = eval_all_pairs, normalize = normalize, norm_scalers = norm_scalers)
+            BO_results = bo_iter(BO_iters,train_p_scl,train_y,theta_set_scl,Theta_True_scl,train_iter,explore_bias, Xexp_scl, Yexp, noise_std, obj, i, sparse_grid, emulator, set_lengthscale, true_model_coefficients_scl, param_dict, verbose, save_fig, runs, DateTime, test_p_scl, sep_fact = sep_fact, LHS = LHS, skip_param_types = skip_param_types, eval_all_pairs = eval_all_pairs, normalize = normalize, norm_scalers = norm_scalers, case_study = case_study)
         else:
-            BO_results = bo_iter(BO_iters,train_p,train_y,theta_set,Theta_True,train_iter,explore_bias, Xexp, Yexp, noise_std, obj, i, sparse_grid, emulator, set_lengthscale, true_model_coefficients, param_dict, verbose, save_fig, runs, DateTime, test_p, sep_fact = sep_fact, LHS = LHS, skip_param_types = skip_param_types, eval_all_pairs = eval_all_pairs, normalize = normalize, norm_scalers = None)
+            BO_results = bo_iter(BO_iters,train_p,train_y,theta_set,Theta_True,train_iter,explore_bias, Xexp, Yexp, noise_std, obj, i, sparse_grid, emulator, set_lengthscale, true_model_coefficients, param_dict, verbose, save_fig, runs, DateTime, test_p, sep_fact = sep_fact, LHS = LHS, skip_param_types = skip_param_types, eval_all_pairs = eval_all_pairs, normalize = normalize, norm_scalers = None, case_study = case_study)
         
         #Add all SSE/theta results at each BO iteration for that run
         Theta_Best_matrix[i,:,:] = BO_results[0]

bo_methods_lib/CS2_create_data.py
@@ -228,7 +228,7 @@ def create_y_data(param_space, true_model_coefficients, x, skip_param_types = 0,
    
     return y_sim
 
-def gen_y_Theta_GP(x_space, Theta, true_model_coefficients, skip_param_types = 0, norm_scalers = None):
+def gen_y_Theta_GP(x_space, Theta, true_model_coefficients, p_true, CS, skip_param_types = 0, norm_scalers = None, emulator = True):
 # def gen_y_Theta_GP(x_space, Theta):
     """
     Generates an array of Best Theta Value and X to create y data
@@ -257,15 +257,17 @@ def gen_y_Theta_GP(x_space, Theta, true_model_coefficients, skip_param_types = 0
         norm = False
         m = x_space.shape[0]
         scaler_x, scaler_theta, scaler_C_before, scaler_C_after = norm_scalers
-        true_model_coefficients_unscl = normalize_constants(Constants, p_true, scaler_theta, skip_params, CS, norm, scaler_C_before, scaler_C_after)[0]
+        true_model_coefficients_unscl = normalize_constants(true_model_coefficients, p_true, scaler_theta, skip_param_types, CS, norm, scaler_C_before, scaler_C_after)[0]
         Theta_unscl = normalize_p_set(clean_1D_arrays(Theta, param_clean = True), scaler_theta, norm)[0]
         x_space_unscl = normalize_x(x_space, m, x_space, emulator, norm, scaler_x)[0]
+        
         Theta_use = Theta_unscl
         x_space_use = x_space_unscl
+        true_model_coefficients_use = true_model_coefficients_unscl
     else:
         Theta_use = Theta
         x_space_use = x_space
-        true_model_coefficients_use = true_model_coefficients_unscl
+        true_model_coefficients_use = true_model_coefficients
     
     m = x_space.shape[1]
     q = Theta.shape[0]
@@ -372,7 +374,10 @@ def make_next_point(train_p, train_y, theta_b, Xexp, Yexp, emulator, true_model_
     """
     #Note train_p is already unscaled
     #Unscale for Data Generation
-    q = train_p.shape[1] - Xexp.shape[1]
+    if emulator == True:
+        q = train_p.shape[1] - Xexp.shape[1]
+    else:
+        q = train_p.shape[1]
     true_p_shape = np.zeros(q)
     
     if str(norm_scalers) != "None":
@@ -383,7 +388,7 @@ def make_next_point(train_p, train_y, theta_b, Xexp, Yexp, emulator, true_model_
         scaler_x, scaler_theta, scaler_C_before, scaler_C_after = norm_scalers
         theta_b_unscl = normalize_p_true(theta_b, scaler_theta, norm)
         Xexp_unscl = normalize_x(Xexp, m, Xexp, emulator, norm, scaler_x)[0]
-        true_model_coefficients_unscl = normalize_constants(Constants, true_p_shape, scaler_theta, skip_param_types, CS, norm, scaler_C_before, scaler_C_after)[0]
+        true_model_coefficients_unscl = normalize_constants(true_model_coefficients, true_p_shape, scaler_theta, skip_param_types, CS, norm, scaler_C_before, scaler_C_after)[0]
         theta_b_use = theta_b_unscl
         Xexp_use = Xexp_unscl
         true_model_coefficients_use = true_model_coefficients_unscl