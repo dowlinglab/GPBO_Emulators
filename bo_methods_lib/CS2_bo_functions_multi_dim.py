e4503671a83e9acfeb730141e2e72d2fffaf1a32
Montana Carlozo <mcarlozo@nd.edu>
2022-12-15 12:20:15 -0500

    Attempted to look for reasons why 8D case study doesn't work well

--- bo_methods_lib/CS2_bo_functions_multi_dim.py
+++ bo_methods_lib/CS2_bo_functions_multi_dim.py
@@ -1274,7 +1274,7 @@ def bo_iter_w_runs(BO_iters,all_data_doc,t,theta_set,Theta_True,train_iter,explo
 #             print(norm_vals, norm_scalers)
         else:
             norm_scalers = None
-            bounds_p_scl, train_p_scl, test_p_scl, bounds_x_scl, Xexp_scl, theta_set_scl, Theta_True_scl, true_model_coefficients_scl =  bounds, train_p, test_p, Xexp, theta_set, Theta_True, true_model_coefficients
+            bounds_p_scl, train_p_scl, test_p_scl, bounds_x_scl, Xexp_scl, theta_set_scl, Theta_True_scl, true_model_coefficients_scl =  bounds_p, train_p, test_p, bounds_x, Xexp, theta_set, Theta_True, true_model_coefficients
         
         #Testing that normalization is happening correctly
 #         norm = False
