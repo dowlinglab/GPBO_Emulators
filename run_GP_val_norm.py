Fixed bugs in normalization LOO code and reran jobs

run_GP_val_norm.py
@@ -66,7 +66,7 @@ sep_fact = np.linspace(1,1,1)
 set_lengthscale = None
 explore_bias = 1
 plot_axis = np.array([1,0])
-norm = True
+normalizing = np.array([False,True])
 
 obj = np.array(["obj", "LN_obj"])
 # obj = np.array(["obj"])
@@ -85,19 +85,21 @@ Yexp = exp_data[:,-1]
 
 Xexp = clean_1D_arrays(Xexp)
 
-for emul in emulator:
-    print("Emulator =", emul)
-    if emul == False:
-        t_use = t
-        obj_use = obj
-    else:
-        t_use = t*n
-        obj_use = np.array(["obj"])
-    for obj_func in obj_use:
-        print("Objective Function =", obj_func)
-        all_data_doc = find_train_doc_path(emul, obj_func, d, t_use)
-        all_data = np.array(pd.read_csv(all_data_doc, header=0,sep=","))
-        LOO_Analysis(all_data, Xexp, Yexp, Constants, true_p, emul, obj_func, CS,  
-                     skip_param_types = skip_param_types, noise_std = noise_std, DateTime = DateTime, 
-                     save_figure= save_figure, plot_axis = plot_axis, normalize = norm, bounds_p = bounds_p, 
-                     bounds_x = bounds_x)        
+for norm in normalizing:
+    print("Norm:", norm)
+    for emul in emulator:
+        print("Emulator =", emul)
+        if emul == False:
+            t_use = t
+            obj_use = obj
+        else:
+            t_use = t*n
+            obj_use = np.array(["obj"])
+        for obj_func in obj_use:
+            print("Objective Function =", obj_func)
+            all_data_doc = find_train_doc_path(emul, obj_func, d, t_use)
+            all_data = np.array(pd.read_csv(all_data_doc, header=0,sep=","))
+            LOO_Analysis(all_data, Xexp, Yexp, Constants, true_p, emul, obj_func, CS,  
+                         skip_param_types = skip_param_types, noise_std = noise_std, DateTime = DateTime, 
+                         save_figure= save_figure, plot_axis = plot_axis, normalize = norm, bounds_p = bounds_p, 
+                         bounds_x = bounds_x)        