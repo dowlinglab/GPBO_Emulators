ac8d4cfc0e8bc11f76ac35e8b5a5a7aa232ef0cb
Montana Carlozo <mcarlozo@nd.edu>
2023-02-24 11:30:08 -0500

    Ran LOO in theta jobs

--- run_GP_val_Theta.py
+++ run_GP_val_Theta.py
@@ -20,7 +20,7 @@ timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S)")
 print("Date and Time: ", timestampStr)
 # DateTime = dateTimeObj.strftime("%Y/%m/%d/%H-%M-%S%p")
 DateTime = dateTimeObj.strftime("%Y/%m/%d/%H-%M")
-# DateTime = "2023/2/24/11-10" #If you want to set a specific time
+# DateTime = "2023/2/24/11-11" #If you want to set a specific time
 # DateTime = None ##For Testing
 print("Date and Time Saved: ", DateTime)
 
