2f792607d60c43c59f6a73f6e341cdc39733dccf
Montana Carlozo <mcarlozo@nd.edu>
2023-03-14 15:10:55 -0400

    Added file saving abilities and fixed bugs in GP vs true movie creator

--- bo_methods_lib/GP_Vs_True_Sensitivity.py
+++ bo_methods_lib/GP_Vs_True_Sensitivity.py
@@ -23,7 +23,7 @@ from .CS2_create_data import gen_y_Theta_GP, calc_y_exp, create_y_data
 ###Load data
 ###Get constants
 ##Note: X and Y should be 400 points long generated from meshgrid values and calc_y_exp :)
-def Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, true_model_coefficients, true_p, obj, Case_Study, bounds_p, skip_param_types = 0, set_lengthscale = None, train_iter = 300, noise_std = 0.1, verbose = False, DateTime = None, save_figure= True, eval_Train = False):  
+def Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, true_model_coefficients, true_p, Case_Study, bounds_p, skip_param_types = 0, set_lengthscale = None, train_iter = 300, noise_std = 0.1, verbose = False, DateTime = None, save_csvs = True, save_figure= False, eval_Train = False, CutBounds = False):  
     """
     Run GP Validation using a leave one out scheme
     
@@ -81,6 +81,9 @@ def Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, true_model_coefficients
 #     print(train_p.dtype, train_y.dtype)
     train_GP = train_GP_model(model, likelihood, train_p, train_y, train_iter, verbose=verbose)
 
+    #Create list to save evaluated arrays in
+    eval_p_df = []
+#     
     #Evaluate GP with true parameters OR a close training point over meshgrid X1 X2
     if eval_Train == False:
         eval_p_base = true_p
@@ -90,17 +93,27 @@ def Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, true_model_coefficients
 #         eval_p = torch.tensor([1.05, -1.61, -7.16, -1.39, -0.47,  1.68, 14.31,  0.04]) #For TP = 100
 #         print(len(eval_p))
     print("Base Theta Train for Movies:", eval_p_base)
+    X_space_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = "/X_space_unmeshed", CutBounds = CutBounds, Mul_title = "")
+    save_csv(X_space, X_space_path, ext = "npy")
+#     print("X_space_path", X_space_path)
     #Loop over number of thetas
     for i in range(len(eval_p_base)):   
-        # Evaluate at the lower bound for each theta and add +10% per iter to the upper bound of theta
-        percentiles = np.linspace(0,1.0,11)
+        # Evaluate at the lower bound for each theta and add or subtract 100% of org value from theta to see what happens
+        #Note: Could modify this to be dependent on problem bounds
+        percentiles = np.linspace(-1.0,1.0,41)
+        print(percentiles)
         eval_p = eval_p_base.clone()
         for j in range(len(percentiles)):
-            lower_theta = bounds_p[0,i]
-            upper_theta = bounds_p[1,i]
-            eval_p[i] = lower_theta+(upper_theta - lower_theta)*percentiles[j]
-        
-            eval_components = eval_GP_x_space(eval_p, X_space, train_y, true_model_coefficients, model, likelihood, verbose, set_lengthscale, train_p = train_p, obj = obj, skip_param_types = skip_param_types, noise_std = noise_std, CS = Case_Study)
+#             lower_theta = bounds_p[0,i]
+#             upper_theta = bounds_p[1,i]
+            base_theta = eval_p_base[i] 
+            new_eval_p = base_theta + base_theta*percentiles[j]
+#             print("org theta",eval_p_base[i],"new theta", new_eval_p)
+            eval_p[i] = new_eval_p
+#             print("Eval_p: \n", eval_p)
+            eval_p_df.append(list(eval_p.numpy()))
+#             print("eval_p_df: \n", eval_p_df)
+            eval_components = eval_GP_x_space(eval_p, X_space, train_y, true_model_coefficients, model, likelihood, verbose, set_lengthscale, train_p = train_p, skip_param_types = skip_param_types, noise_std = noise_std, CS = Case_Study)
 
             GP_mean, GP_stdev, y_sim = eval_components
 
@@ -114,31 +127,36 @@ def Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, true_model_coefficients
                               [0.22,0.30]])
 
                 if eval_Train == False:
-                    title1 = "- True Values"
+                    title1 = "True Values"
 #                     print("True Theta" + eval_p.tolist())
                 else:
 #                     print("Test TP Rounded:", np.round(eval_p.tolist(),2))
         #             print(eval_p)
                     theta_eval = np.round(eval_p.tolist(),2)
-                    title1 = r'$' + param_dict[i] + "=" + str(theta_eval[i]) + '$'
+                    title1 = r'$' + param_dict[i] + "=" + str(theta_eval[i]) + '$' 
 #                     print(title1)
                 X_mesh = X_space.reshape(p,p,-1).T
-#                 Muller_plotter(X_mesh, y_sim.T, minima, saddle, title1, X_train = X_train)
+                Muller_plotter(X_mesh, y_sim.T, minima, saddle, title1, set_lengthscale, t, Case_Study, CutBounds, DateTime, X_train, save_csvs, save_figure, Mul_title = "/Sim_val", param = param_dict[i], percentile = percentiles[j])
 
                 #Plot GP shape
-                title2 = "- GP Mean"
-                Muller_plotter(X_mesh, GP_mean.T, minima, saddle, title2, X_train = X_train)
+                title2 = "GP Mean"
+                Muller_plotter(X_mesh, GP_mean.T, minima, saddle, title2, set_lengthscale, t, Case_Study, CutBounds, DateTime, X_train, save_csvs, save_figure, Mul_title = "/GP_mean", param = param_dict[i], percentile = percentiles[j])
 
                 #Plot GPstdev
-                title3 = "- GP StDev"
-                Muller_plotter(X_mesh, GP_stdev.T, minima, saddle, title3, X_train = X_train)
+                title3 = "GP StDev"
+                Muller_plotter(X_mesh, GP_stdev.T, minima, saddle, title3, set_lengthscale, t, Case_Study, CutBounds, DateTime, X_train, save_csvs, save_figure, Mul_title = "/GP_stdev", param = param_dict[i], percentile = percentiles[j])
 
             elif Case_Study == 1:
                 plot_xy(X_space, Xexp, Yexp, None ,GP_mean, y_sim,title = "XY Comparison")
 
+#         print("eval_p_df loop:", eval_p_df)
+    eval_p_df = pd.DataFrame(eval_p_df, columns = list(param_dict.values()))
+    eval_p_df_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = "/eval_p_df", CutBounds = CutBounds, Mul_title = "")
+    save_csv(eval_p_df, eval_p_df_path, ext = "npy")
+#     print("eval_p_df_path", eval_p_df_path)
     return
 
-def eval_GP_x_space(theta_set, X_space, train_y, true_model_coefficients, model, likelihood, verbose, set_lengthscale, train_p = None, obj = "obj", skip_param_types = 0, noise_std = 0.1, CS = 1):
+def eval_GP_x_space(theta_set, X_space, train_y, true_model_coefficients, model, likelihood, verbose, set_lengthscale, train_p = None, skip_param_types = 0, noise_std = 0.1, CS = 1):
     """
     Evaluates GP
     
@@ -278,7 +296,7 @@ def eval_GP_emulator_x_space(theta_set, X_space, true_model_coefficients, model,
     
     return GP_mean, GP_stdev, y_sim
 
-def Muller_plotter(test_mesh, z, minima, saddle, title, X_train = None):
+def Muller_plotter(test_mesh, z, minima, saddle, title, set_lengthscale, t, Case_Study, CutBounds, DateTime = None, X_train = None, save_csvs = False, save_figure = False, Mul_title = "", param = "", percentile = ""):
     '''
     Plots heat maps for 2 input GP
     Parameters
@@ -303,6 +321,7 @@ def Muller_plotter(test_mesh, z, minima, saddle, title, X_train = None):
     -------
         plt.show(), A heat map of test_mesh and z
     '''
+    fxn = "Muller_plotter"
     xx , yy = test_mesh #NxN, NxN
     #Assert sattements
     assert isinstance(z, np.ndarray)==True or torch.is_tensor(z)==True, "The values in the heat map must be numpy arrays or torch tensors."
@@ -323,7 +342,7 @@ def Muller_plotter(test_mesh, z, minima, saddle, title, X_train = None):
     #plot saddle pts and local minima, only label 1st instance
     if str(X_train) != "None":
         plt.scatter(X_train[:,0], X_train[:,1], color = "goldenrod", label = "Training", marker = "o")
-    
+        
     for i in range(len(minima)):
         if i == 0:
             plt.scatter(minima[i,0], minima[i,1], color="black", label = "Minima", s=25, marker = (5,1))
@@ -348,13 +367,37 @@ def Muller_plotter(test_mesh, z, minima, saddle, title, X_train = None):
     plt.ylabel('$x_2$',weight='bold')
     plt.xlim((np.amin(xx), np.amax(xx)))
     plt.ylim((np.amin(yy),np.amax(yy)))
-    plt.title("Mul. Pot. "+title, weight='bold',fontsize=16)
+    plt.title("Mul. Pot. - "+title, weight='bold',fontsize=16)
     
     #Save CSVs and Figures
-           
+    if save_csvs == True:
+        csv_ends = ["/Mul_Pot", "/Minima", "/Saddle", "/X_train"]
+        z_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = csv_ends[0], CutBounds = CutBounds, Mul_title = Mul_title, param = param, percentile = percentile)
+#         print("z_path", z_path)
+        min_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = csv_ends[1], CutBounds = CutBounds, Mul_title = Mul_title, param = param, percentile = percentile)
+#         print("min_path", min_path)
+        sad_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = csv_ends[2], CutBounds = CutBounds, Mul_title = Mul_title, param = param, percentile = percentile)
+#         print("sad_path", sad_path)
+        x_trn_path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = False, csv_end = csv_ends[3], CutBounds = CutBounds, Mul_title = Mul_title, param = param, percentile = percentile)
+#         print("x_trn_path", x_trn_path)
+        csv_item_list = [z, minima, saddle, X_train]
+        make_csv_list = [z_path, min_path, sad_path, x_trn_path]
+
+        for i in range(len(make_csv_list)):
+            save_csv(csv_item_list[i], make_csv_list[i], ext = "npy")
+
+    #Save figure or show and close figure
+    if save_figure == True:
+        path = path_name_gp_val(set_lengthscale, t, Case_Study, DateTime, is_figure = True, CutBounds = CutBounds, Mul_title = Mul_title, param = param, percentile = percentile)
+#         print("fig_path", path)
+        save_fig(path, ext='png', close=True, verbose=False) 
+    else:
+        plt.show()
+        plt.close()
+    plt.close()       
     return plt.show() 
 
-def path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTime = None, is_figure = True, csv_end = None, plot_axis = None, plot_num = None):
+def path_name_gp_val(set_lengthscale, t, Case_Study, DateTime = None, is_figure = True, csv_end = None, CutBounds = False, Mul_title = "", param = "", percentile = ""):
     """
     names a path
     
@@ -377,32 +420,25 @@ def path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTim
         path: str, The path to which the file is saved
     
     """
-
-    obj_str = "/"+str(obj)
     len_scl = "/len_scl_varies"
     org_TP_str = "/TP_"+ str(t)
     CS = "/CS_" + str(Case_Study) 
     
-    if plot_axis == 1 or plot_axis == 0:
-        parity_end = "/axis_val_" +str(plot_axis) + "/plot_num_" + str(plot_num).zfill(len(str(t)))
+    if param != "":
+        param = "/" + str(param)
+    
+    if percentile != "":
+        percent = "/pct_" + str(np.round(float(percentile),3))
     else:
-        parity_end = "/sse_parity_plot"
-
-        
-    if emulator == False:
-        Emulator = "/GP_Error_Emulator"
-        method = ""
-    else:
-        Emulator = "/GP_Emulator"
+        percent = ""
             
-    fxn_dict = {"LOO_Plots_2_Input":"/SSE_gp_val" , "LOO_Plots_3_Input":"/y_gp_val", "LOO_parity_plot_emul":"/parity_plots"+parity_end}
-    plot = fxn_dict[fxn]        
+    plot = "/Mul_Pot_Comp" + Mul_title        
       
     if DateTime is not None:
 #         path_org = "../"+DateTime #Will send to the Datetime folder outside of CS1
         path_org = DateTime #Will send to the Datetime folder outside of CS1
     else:
-        path_org = "Test_Figs_GP_Val"
+        path_org = "Test_Figs"
         
 #         path_org = "Test_Figs"+"/Sep_Analysis2"+"/Figures"
     if is_figure == True:
@@ -410,9 +446,14 @@ def path_name_gp_val(emulator, fxn, set_lengthscale, t, obj, Case_Study, DateTim
     else:
         path_org = path_org + "/CSV_Data"
         
-    path_end = CS + Emulator + org_TP_str + obj_str + len_scl + plot     
+    path_end = CS + org_TP_str + len_scl + plot + param + percent  
 
-    path = path_org + "/GP_Validation_Figs" + path_end 
+    if CutBounds == True:
+        cut_bounds = "_CB"
+    else:
+        cut_bounds = ""
+        
+    path = path_org + "/GP_Vs_Sim_Comp" + cut_bounds + path_end 
         
     if csv_end is not None:
         path = path + csv_end
---
+++ run_GP_vs_True.py
@@ -0,0 +1,109 @@
+import sys
+import gpytorch
+import numpy as np
+import pandas as pd
+import torch
+from datetime import datetime
+from scipy.stats import qmc
+
+# from bo_methods_lib.GP_Vs_True import Compare_GP_True
+from bo_methods_lib.GP_Vs_True_Sensitivity import Compare_GP_True_Movie
+# from bo_methods_lib.GP_Validation import LOO_Analysis
+from bo_methods_lib.bo_functions_generic import gen_theta_set,gen_x_set, find_train_doc_path, set_ep, clean_1D_arrays
+
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 300
+
+import warnings
+warnings.simplefilter("ignore", category=RuntimeWarning)
+
+#Set Date and Time
+dateTimeObj = datetime.now()
+timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S)")
+print("Date and Time: ", timestampStr)
+# DateTime = dateTimeObj.strftime("%Y/%m/%d/%H-%M-%S%p")
+DateTime = dateTimeObj.strftime("%Y/%m/%d/%H-%M")
+print("Date and Time Saved: ", DateTime)
+# DateTime = None ##For Testing
+
+#Set Parameters
+#Need to run at a and b, need 2 arrays to test that this will work
+CS = 2.2
+
+Bound_Cut = True
+eval_Train = True
+
+Constants = np.array([[-200,-100,-170,15],
+                      [-1,-1,-6.5,0.7],
+                      [0,0,11,0.6],
+                      [-10,-10,-6.5,0.7],
+                      [1,0,-0.5,-1],
+                      [0,0.5,1.5,1]])
+if CS == 2.2:
+    skip_param_types = 1 #This is what changes for subpoint
+    true_p = Constants[skip_param_types:skip_param_types+2].flatten()
+    param_dict = {0 : 'a_1', 1 : 'a_2', 2 : 'a_3', 3 : 'a_4',
+                  4 : 'b_1', 5 : 'b_2', 6 : 'b_3', 7 : 'b_4'}
+    exp_d = 2
+    if Bound_Cut == True:
+        bounds_x = np.array([[-1.0, 0.0],
+                            [   0.5, 1.5]])
+        n = 25 #Number of experimental data points to use
+    else:    
+        bounds_x = np.array([[-1.5, -0.5],
+                     [   1,    2]])
+        n = 27 #Number of experimental data points to use
+    bounds_p = np.array([[-2, -2, -10, -2, -2, -2,  5, -2],
+                   [ 2,  2,   0,  2,  2,  2, 15,  2]])
+
+else:
+    Constants = true_p = np.array([1,-1])
+    skip_param_types = 0
+    param_dict = {0 : '\\theta_1', 1 : '\\theta_2'}
+    exp_d = 1
+    n = 5
+    bounds_x = np.array([[-2], [2]])
+    bounds_p = np.array([[-2, -2],
+                         [ 2,  2]])
+
+# print(Theta_True)
+t_list = [40]
+d = len(true_p)
+train_iter = 300
+noise_std = 0.1
+set_lengthscale = None
+verbose = False
+
+emulator =  True
+save_figure = True
+save_csvs = True
+
+if Bound_Cut == True:
+    cut_bounds = '_cut_bounds'
+else:
+    cut_bounds = ""
+
+#Pull Experimental data from CSV
+exp_data_doc = 'Input_CSVs/Exp_Data/d='+str(exp_d)+'/n='+str(n)+cut_bounds+'.csv'
+exp_data = np.array(pd.read_csv(exp_data_doc, header=0,sep=","))
+Xexp = exp_data[:,1:exp_d+1]
+Yexp = exp_data[:,-1]
+
+Xexp = clean_1D_arrays(Xexp)
+
+#Define GP Testing space
+p=20
+# print(bounds_x)
+X_space = gen_x_set(LHS = False, n_points = p, dimensions = exp_d, bounds = bounds_x)
+# print(X_space[-5:,:])
+
+for t in t_list:
+    t_use = int(t*n)
+    all_data_doc = find_train_doc_path(emulator, obj, d, t_use, bound_cut = Bound_Cut)
+    print(all_data_doc)
+#     Compare_GP_True(all_data, X_space, Xexp, Yexp, Constants, true_p, obj, CS, 
+#                 skip_param_types, set_lengthscale, train_iter, noise_std, verbose, DateTime, 
+#                 save_figure, eval_Train = eval_Train)
+    Compare_GP_True_Movie(all_data, X_space, Xexp, Yexp, Constants, true_p, CS, bounds_p,
+                skip_param_types, set_lengthscale, train_iter, noise_std, verbose, DateTime, 
+                save_csvs, save_figure, eval_Train = eval_Train, CutBounds = Bound_Cut)
