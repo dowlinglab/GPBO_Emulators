8e1f4d811b5cefc3da9866f4e2017eeb957bf444
Montana Carlozo <mcarlozo@nd.edu>
2023-05-18 12:34:53 -0400

    Added classes for plotting, problem definition, and data savig to .npy files

--- bo_methods_lib/GPBO_Classes.py
+++ bo_methods_lib/GPBO_Classes.py
@@ -30,7 +30,7 @@ from .CS2_bo_plotters import value_plotter, plot_xy, plot_Theta, plot_Theta_min,
 
 class Case_Study:
     """
-    The base class for any GPBO Method
+    The base class for any GPBO Case Study and Evaluation
     Parameters
     
     Methods
@@ -41,15 +41,28 @@ class Case_Study:
     """
     # Class variables and attributes
     
-    def __init__(self, CS_name):
+    def __init__(self, cs_name, ep0, sep_fact, normalize, num_data, bo_iter_tot, bo_run_tot):
         """
         Parameters
         ----------
-        method_name, str, The name associated with the case study being evaluated
+        cs_name, str, The name associated with the case study being evaluated
+        ep0: float, The original  exploration bias. Default 1
+        sep_fact: float or int, The separation factor that decides what percentage of data will be training data. Between 0 and 1.
+        normalize: bool, Determines whether feature data will be normalized for problem analysis
+        num_data: int, number of available data for training/testing
+        bo_iter_tot: int, total number of BO iterations per restart
+        bo_run_tot: int, total number of BO algorithm restarts
         """
         # Constructor method
-        self.method_name = method_name
-    def define_method(self):
+        self.cs_name = cs_name
+        self.ep0 = ep0
+        self.sep_fact = sep_fact
+        self.normalize = normalize
+        self.num_data = num_data
+        self.bo_iter_tot = bo_iter_tot
+        self.bo_run_tot = bo_run_tot
+        
+    def define_cs(self):
         """
         Returns:
         ---------
@@ -58,9 +71,8 @@ class Case_Study:
         param_dict: dictionary, dictionary of names of each parameter that will be plotted named by indecie w.r.t Theta_True
         skip_param_types: int, The offset of which parameter types (A - y0) that are being guessed. Default 0
         """
-        #Logic for determining obj, sparse_grid, and emulator
 
-
+        
 class GPBO_Methods:
     """
     The base class for any GPBO Method
@@ -82,6 +94,7 @@ class GPBO_Methods:
         """
         # Constructor method
         self.method_name = method_name
+        
     def define_method(self):
         """
         Parameters:
@@ -94,6 +107,7 @@ class GPBO_Methods:
         GP_training_dims: int, Number of features for GP
         """
         #Logic for determining obj, sparse_grid, and emulator
+        
     
 class Parameters_And_State_Points:
     """
@@ -166,7 +180,8 @@ class Parameters_And_State_Points:
         # Method definition
         # Code logic goes here
         pass
-    
+ 
+
 class Parameter_Sets(Parameters_And_State_Points):
     """
     The base class for any Parameter Set that will be used to train or evaluate a GP
@@ -183,7 +198,8 @@ class Parameter_Sets(Parameters_And_State_Points):
     
     """
     # Class variables and attributes
-    def __init__(self, Case_Study, Method, bounds_p, bounds_x):
+    
+    def __init__(self, Case_Study, Method, bounds_p, bounds_x): #Am I able to initialize a class with another class? I need the params from it.
         """
         Parameters
         -----------
@@ -193,7 +209,7 @@ class Parameter_Sets(Parameters_And_State_Points):
         bounds_x: ndarray, The bounds of Xexp
         """
         # Constructor method
-        self.Case_Study = Case_Study
+        self.Case_Study = Case_Study 
         self.Method = Method
         super().__init__(bounds_p, bounds_x)
         
@@ -215,16 +231,15 @@ class Parameter_Sets(Parameters_And_State_Points):
         # Code logic goes here
         pass
     
-    
-    def train_test_split(self, point, sep_fact, seed = None):
+    def train_test_split(self, point, seed = None):
         """
         Splits available data into training and tetsing data
         
         Parameters
         ----------
         param_set: ndarray (n_points x n_dims) matrix of parameter sets which are available as training data
-        sep_fact: float or int, The separation factor that decides what percentage of data will be training data. Between 0 and 1.
         seed: None or int, Optional Seed for test/train split
+        #Note - Separation factor inherited from Case_Study
         
         Returns:
         ---------
@@ -254,7 +269,7 @@ class Parameter_Sets(Parameters_And_State_Points):
         # Code logic goes here
         pass
     
-    def optimize_set(self, param_set, Yexp, train_data, model, likelihood, Method, Case_Study, ep, derived_val):
+    def optimize_set(self, param_set, Yexp, train_data, model, likelihood, Case_Study, Method, ep, derived_val):
         """
         Optimizes theta set to find best SSE or EI
         
@@ -265,8 +280,8 @@ class Parameter_Sets(Parameters_And_State_Points):
         train_data: ndarray, The GP training data
         model: bound method, The model that the GP is bound by
         likelihood: bound method or None, The likelihood of the GP model. In this case, must be a Gaussian likelihood
-        Method: class, The method with which to evaluate the GP
         Case_Study: class, The case study which the method is applied to
+        Method: class, The method with which to evaluate the GP
         ep: float, The exploration bias used for calculation of EI
         derived_val: ndarray, either "SSE" or "EI". The array values we want to optimize over theta
         ---------
@@ -275,6 +290,7 @@ class Parameter_Sets(Parameters_And_State_Points):
         # Method definition
         # Code logic goes here
         pass
+    
     def create_y_data(self, param_set, Case_Study, Method, noise_std, noise_mean = 0, seed = None):
         """
         Creates y data for a given case study and method
@@ -282,11 +298,13 @@ class Parameter_Sets(Parameters_And_State_Points):
         Parameters
         ----------
         param_set: ndarray (n_points x n_dims) matrix of parameter sets to evaluate
-        Method: class, The method with which to evaluate the GP
         Case_Study: class, The case study which the method is applied to
+        Method: class, The method with which to evaluate the GP
         noise_std: float, int: The standard deviation of the noise
         noise_mean: float, int: The mean of the noise. Default 0
         seed: None or int, Optional Seed for noise generation. Default None
+        
+        Returns
         ---------
         y_data: ndarray, the y data associated with a given parameter set
         """
@@ -302,8 +320,8 @@ class Parameter_Sets(Parameters_And_State_Points):
         ----------
         param_set: ndarray (n_points x n_dims) matrix of parameter sets to evaluate
         Yexp: ndarray, Experimental output data
-        Method: class, The method with which to evaluate the GP
         Case_Study: class, The case study which the method is applied to
+        Method: class, The method with which to evaluate the GP
         noise_std: float, int: The standard deviation of the noise
         noise_mean: float, int: The mean of the noise. Default 0
         seed: None or int, Optional Seed for noise generation. Default None
@@ -313,7 +331,8 @@ class Parameter_Sets(Parameters_And_State_Points):
         # Method definition
         # Code logic goes here
         pass
-    
+  
+
 class Hyperparameters:
     """
     The base class for any best error
@@ -321,8 +340,8 @@ class Hyperparameters:
     Methods
     --------------
     __init__
-    calc_be()
-
+    set_hps()
+    print_hps
     """
     # Class variables and attributes
     
@@ -337,7 +356,6 @@ class Hyperparameters:
         # Constructor method
         self.hp_name = hp_name
         self.constant = constant
-        self.value = value
         
     def set_hps(self, value = 1):
         """
@@ -366,6 +384,7 @@ class Hyperparameters:
         hyperparam_pp: str, The hyperparameter as a string rounded appropriately
         """
         #Logic 
+
         
 class Exploration_Bias(ep_bias = 1):
     """
@@ -378,7 +397,7 @@ class Exploration_Bias(ep_bias = 1):
     
     Variables
     --------------
-    ep0: float,  The original  exploration bias. Default 1
+    ep0: float, The original exploration bias. Default 1
     """
     # Class variables and attributes
     ep0 = ep_bias
@@ -387,7 +406,8 @@ class Exploration_Bias(ep_bias = 1):
         """
         Parameters
         ----------
-        method_name, str, The name associated with the method being tested
+        ep_calc_method: str, The name associated with the exploration parameter method being tested
+        ep: float, the current exploration parameter value
         """
         # Constructor method
         self.ep_calc_method = ep_calc_method
@@ -402,7 +422,7 @@ class Exploration_Bias(ep_bias = 1):
         Bo_iter: int, The value of the current BO iteration
         mean_of_var: float, The value of the average of all posterior variances
         best_error: float, The best error of the GP
-        e_inc: float, the increment for the Boyle's method for calculating exploration parameter: Default is 1.5
+        e_inc: float, The increment for the Boyle's method for calculating exploration parameter: Default is 1.5
         ep_f: float, The final exploration parameter value: Default is 0.01
         ep_method: float, determines if Boyle, Jasrasaria, or exponential method will be used: Defaults to constant ep
         improvement: Bool, Determines whether last objective was an improvement
@@ -412,7 +432,8 @@ class Exploration_Bias(ep_bias = 1):
         ep: float, The current value of exploration bias
         """
         #Logic
-        
+  
+
 class Best_Error:
     """
     The base class for any best error
@@ -429,18 +450,18 @@ class Best_Error:
         """
         Parameters
         ----------
-        method_name, str, The name associated with the method being tested
+        .....
         """
         # Constructor method
         
-    def calc_be(self, Method, Case_Study, train_data, Xexp, Yexp):
+    def calc_be(self, Case_Study, Method, train_data, Xexp, Yexp):
         """
         Evaluates value of exploration parameter at each BO iteration
         
         Parameters:
         -----------
-        Method: class, The method with which to evaluate the GP
         Case_Study: class, The case study which the method is applied to
+        Method: class, The method with which to evaluate the GP
         train_data: ndarray, The GP training data
         Xexp: ndarray, Experimental input data
         Yexp: ndarray, Experimental output data
@@ -450,6 +471,7 @@ class Best_Error:
         best_error: float, The best error of the GP model
         """
         #Logic for determining be 
+
         
 class GP_Derived_Values:
     """
@@ -458,7 +480,10 @@ class GP_Derived_Values:
     Methods
     --------------
     __init__
-    eval_GP_param()
+    calc_ei()
+    find_argmax_param_set()
+    calc_sse()
+    find_argmin_param_set()
 
     """
     # Class variables and attributes
@@ -472,7 +497,7 @@ class GP_Derived_Values:
         # Constructor method
         self.derived_val_name = derived_val_name
         
-    def calc_EI(self, Method, train_data, Xexp, Yexp, model_mean, model_variance, ep):
+    def calc_ei(self, Method, train_data, Xexp, Yexp, model_mean, model_variance, ep):
         """
         Evaluates value of expected improvement
         
@@ -536,6 +561,150 @@ class GP_Derived_Values:
         argmin_param_set: ndarray, The parameter sets associated with the argmin of the derived property value
         """ 
         
+def Plotter_Class:
+    """
+    The base class for making plots
+    
+    Methods
+    --------------
+    __init__
+    plot_heat_map()
+    plot_line_graph()
+    plot_scatter_graph()
+    plot_3d_scatter_graph()
+    
+    """
+    
+    def __init__(self):
+        """
+        Parameters
+        ----------
+        
+        """
+        # Constructor method
+        
+    def plot_heat_map(self, param_vals, param_names, z, z_title, scatter_points, scatter_labels, title = "", levels = 100):
+        """
+        Creates a heat map given a mesh grid (x-y) and corresponding z plot values
+        
+        Parameters
+        ----------
+        param_vals: ndarray, a meshgrid of parameter values to plot on the x and y axes
+        param_names: list, names of parameter values to plot on the x and y axes
+        z: list of ndarray, an array of the value(s) corresponding to the heat maps
+        z_title: list of str, list of titles for what the value of z represents 
+        scatter_points: list of ndarray, list of values of scatter points to add to the heat maps
+        scatter_labels: list of str, list of names of scatter points to add to the heat maps
+        title: str, Title of the graph. Default empty string
+        levels: int, the value of levels to include during heat map plotting (black lines). Default = 100
+        
+        Returns:
+        --------
+        Heat maps of given data
+        """
+
+    def plot_line_graph(self, param_vals, param_names, z, z_title, title = ""):
+        """
+        Creates a line plot given x-axis values and corresponding y plot values
+        
+        Parameters
+        ----------
+        param_vals: ndarray, a meshgrid of parameter values to plot on the x axes
+        param_names: list, names of parameter values to plot on the x axes
+        z: list of ndarray, an array of the value(s) corresponding to the y-axis
+        z_title: list of str, list of titles for what the value of z represents 
+        title: str, Title of the graph. Default empty string
+        
+        Returns:
+        --------
+        Line plots of given data
+        """   
+        
+    def plot_scatter_graph(self, param_vals, param_names, scatter_points, scatter_labels, title = ""):
+        """
+        Creates a scatter plot given x-axis values and corresponding y plot values
+        
+        Parameters
+        ----------
+        param_vals: ndarray, a meshgrid of parameter values to plot on the x axes
+        param_names: list, names of parameter values to plot on the x and y axes
+        scatter_points: list of ndarray, list of values of scatter points to add to the heat maps
+        scatter_labels: list of str, list of names of scatter points to add to the heat maps
+        title: str, Title of the graph. Default empty string
+        
+        Returns:
+        --------
+        Scatter plots of given data
+        """ 
+      
+    def plot_3d_scatter_graph(self, param_vals, param_names, scatter_points, scatter_labels, title = ""):
+         """
+        Creates a scatter plot given x-axis and y-axis values and corresponding z plot values
+        
+        Parameters
+        ----------
+        param_vals: ndarray, a meshgrid of parameter values to plot on the x axes
+        param_names: list, names of parameter values to plot on the x and y axes
+        scatter_points: list of ndarray, list of values of scatter points to add to the heat maps
+        scatter_labels: list of str, list of names of scatter points to add to the heat maps
+        title: str, Title of the graph. Default empty string
+        
+        Returns:
+        --------
+        3D Scatter plots of given data
+        """ 
+    
+    
+class Set_File_Path:
+    """
+    The base class for saving CSVs
+
+    Methods
+    --------------
+    __init__
+    save_data_file()
+    save_plot_figure()
+    """
+
+    def __init__(self):
+        """
+        Parameters
+        ----------
+
+        """
+        # Constructor method
+    
+    def save_data_file(self, DateTime, Case_Study, Method, GP_Model, bo_iter, bo_restart):
+        """
+        Saves data files to a .npy file
+        
+        Parameters
+        ----------
+        DateTime: str or None, Determines whether files will be saved with the date and time for the run, Default None
+        Case_Study: class, The case study being evaluated
+        Method: class, The method being tested
+        GP_model: class, The GP model used in the analysis
+        bo_iter: int, The BO iter number for the problem
+        bo_restart: int, The restart iter number for the problem
+        
+        """        
+        
+    def save_plot_figure(self, DateTime, Case_Study, Method, GP_Model, bo_iter, bo_restart):
+        """
+        Saves figures to a .png file
+        
+        Parameters
+        ----------
+        DateTime: str or None, Determines whether files will be saved with the date and time for the run, Default None
+        Case_Study: class, The case study being evaluated
+        Method: class, The method being tested
+        GP_model: class, The GP model used in the analysis
+        bo_iter: int, The BO iter number for the problem
+        bo_restart: int, The restart iter number for the problem
+        """
+    
+    # Class variables and attributes
+        
 # QUESTIONS:
 #     1) What is the best way to save csvs of the necessary data? How can I write that to a class such that each function doesn't need 100 parameters?
 #     2) Is there a better way to define the Best_Error/GP_Derived_Values classes together? As it is, what would I initialize for Best_Error?
@@ -543,3 +712,5 @@ class GP_Derived_Values:
 #     4) When do I use a function vs a class?
 #     5) Where would I add a method for GP validation? Parameter_Set?
 #     6) Which class should creating data be part of?
+#     7) How should I make a class for plotters given that at any point I may want multiple different objects that would appear on one graph but not another? What would I even initialize for a plotting class?
+
