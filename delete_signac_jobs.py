5cea1513e0634e8a907b32620a30b7a0ede69b5e
Montana Carlozo <mcarlozo@nd.edu>
2023-10-12 12:24:20 -0400

    Added CS2_4 a and y0 studies for restrain_GP =10

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":1}):
+for job in project.find_jobs({'cs_name_val': {'$lt': 2.0}, 'retrain_GP': 10}):
     job.remove() 
