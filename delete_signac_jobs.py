0c352e3323d36e3724d6ad3d8479dd88e9223664
Montana Carlozo <mcarlozo@nd.edu>
2023-11-13 13:11:09 -0500

    Added new jobs for CS3

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({'cs_name_val': 9}):
+for job in project.find_jobs({'cs_name_val': 9, 'bo_iter_tot': 50}):
     job.remove() 
