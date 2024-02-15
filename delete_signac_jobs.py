6f77040c5e32f1da356b7619f770fcaada919430
Montana Carlozo <mcarlozo@nd.edu>
2024-02-15 15:53:58 -0500

    Deleted jobs with ep_enum_val > 1 for CS1

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":10,  'param_name_str': {'$exists': False}}):
+for job in project.find_jobs({"cs_name_val":1,  'param_name_str': {'$exists': False}, "ep_enum_val":{"$in": [2, 3, 4]} }):
     job.remove() 
