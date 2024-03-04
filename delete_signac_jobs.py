3df6fc0288f616b91ddf7301d114cc3c0e0bece6
Montana Carlozo <mcarlozo@nd.edu>
2024-03-04 18:12:04 -0500

    Attempted to debug CS16

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":{"$in": [1, 11, 17]},  'param_name_str': {'$exists': False}, "ep_enum_val":{"$in": [1]} }):
+for job in project.find_jobs({"cs_name_val":{"$in": [1, 11]},  'param_name_str': {'$exists': False}, "ep_enum_val":{"$in": [1]} }):
     job.remove() 
