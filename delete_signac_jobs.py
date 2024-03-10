d43a7cc34de42503282777513713f1b7faaa5fd2
Montana Carlozo <mcarlozo@nd.edu>
2024-03-09 20:52:55 -0500

    Started jobs for CS 11,15,and 17

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":{"$in": [1]},  'gp_package': {'$exists': True}, "ep_enum_val":{"$in": [1]} }):
+for job in project.find_jobs({"cs_name_val":{"$in": [11]},  'gp_package': {'$exists': True}, "ep_enum_val":{"$in": [1]} }):
     job.remove() 
