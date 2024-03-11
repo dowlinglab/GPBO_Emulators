9641b7b56bfc23fde00c1a5d15ce1012cbc209ee
Montana Carlozo <mcarlozo@nd.edu>
2024-03-11 17:54:23 -0400

    Added gp_package statepoint to all jobs

--- add_sp.py
+++ add_sp.py
@@ -2,6 +2,6 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":3, "bo_iter_tot": 75}):
-    assert "bo_run_num" not in job.sp
-    job.update_statepoint({"bo_run_num":int((job.sp.seed-1)/2 + 1)})
+for job in project:
+    if 'gp_package' not in job.statepoint:
+        job.sp.gp_package = "sklearn"
