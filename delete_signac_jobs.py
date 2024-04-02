92a631f3983df548007efec653554f3bce795564
Montana Carlozo <mcarlozo@nd.edu>
2024-04-02 12:35:44 -0400

    Deleted old, and unrun signac jobs for sklearn (which is no longer used)

--- delete_signac_jobs.py
+++ delete_signac_jobs.py
@@ -2,5 +2,5 @@ import signac
 
 project = signac.get_project()
 
-for job in project.find_jobs({"cs_name_val":{"$in": [1]},  'gp_package': "gpflow", "ep_enum_val":{"$in": [1]} }):
+for job in project.find_jobs({"cs_name_val":{"$in": [12]},  'gp_package': "sklearn", "ep_enum_val":{"$in": [1]} }):
     job.remove() 
