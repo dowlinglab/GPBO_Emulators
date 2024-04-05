be494ba36daa815f88010e45457f03bf574cecb1
Montana Carlozo <mcarlozo@nd.edu>
2024-04-05 17:13:37 -0400

    Ran analysis scripts for CS14

--- make_1obj_hms.py
+++ make_1obj_hms.py
@@ -29,7 +29,7 @@ save_figs = True
 
 project = signac.get_project()
 
-for val in [13]:
+for val in [14]:
     criteria_dict = {"cs_name_val" : val,
                     "ep_enum_val": 1,
                     "gp_package":"gpflow",
